cd8aaf4b240aa7e2aeeb7d387241787b84adf967
jakedunahee <jake@getambassador.com>
2015-07-29 19:19:22 -0400

    [#99614782] Added new SDKTest Project

---
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/example/ambassador/ambassadorsdk/Identify.java
@@ -0,0 +1,14 @@
+package com.example.ambassador.ambassadorsdk;
+
+import android.webkit.WebView;
+
+/**
+ * Created by JakeDunahee on 7/23/15.
+ */
+public class Identify {
+    private WebView webView;
+
+    public void identify() {
+        
+    }
+}
