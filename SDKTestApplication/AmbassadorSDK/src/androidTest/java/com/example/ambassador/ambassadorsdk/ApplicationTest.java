f33cb3d66f4c8fa1f35ec8dfca4f363f9f6cd305
Corey Fields <corey@getambassador.com>
2015-08-28 15:28:33 -0400

    [#102093184] tests for ambassador activity

--- SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/ApplicationTest.java
+++ SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/ApplicationTest.java
@@ -1,10 +1,15 @@
 package com.example.ambassador.ambassadorsdk;
 
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.example.ambassador.ambassadorsdk.AmbassadorActivity;
+import com.example.ambassador.ambassadorsdk.RAFParameters;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,10 +28,19 @@ import static org.hamcrest.Matchers.not;
 public class ApplicationTest {
 
     @Rule
-    public ActivityTestRule<AmbassadorActivity> mActivityRule = new ActivityTestRule(AmbassadorActivity.class);
+    public ActivityTestRule<AmbassadorActivity> mActivityRule = new ActivityTestRule(AmbassadorActivity.class, true, false);
 
     @Test
-    public void testOnCreate() {
+    public void demonstrateIntentPrep() {
+        RAFParameters parameters = new RAFParameters();
+        parameters.shareMessage = "Check out this company!";
+        parameters.welcomeTitle = "RAF Params Welcome Title";
+        parameters.welcomeDescription = "RAF Params Welcome Description";
+        parameters.toolbarTitle = "RAF Params Toolbar Title";
 
+        Intent intent = new Intent();
+        intent.putExtra("test", parameters);
+        mActivityRule.launchActivity(intent);
+        //onView(withId(R.id.rlMainLayout)).check(matches(isDisplayed()));
     }
 }
