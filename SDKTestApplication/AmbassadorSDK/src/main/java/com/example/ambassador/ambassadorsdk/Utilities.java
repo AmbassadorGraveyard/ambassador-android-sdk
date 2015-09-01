f1138caf6598c3756d11d03a387bd1b80efca3ac
jakedunahee <jake@getambassador.com>
2015-09-01 16:48:15 -0400

    [#102421564] Added AugurSDK to AmbassadorSDK.  Created new Identity class, as well as IdentyPusher and IdentityAugurSDK classes.

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/example/ambassador/ambassadorsdk/Utilities.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/example/ambassador/ambassadorsdk/Utilities.java
@@ -1,6 +1,7 @@
 package com.example.ambassador.ambassadorsdk;
 
 import android.content.Context;
+import android.content.res.Configuration;
 
 /**
  * Created by JakeDunahee on 8/31/15.
@@ -13,5 +14,15 @@ class Utilities {
     public static int getPixelSizeForDimension(int dimension) {
         Context cxt = MyApplication.getAppContext();
         return cxt.getResources().getDimensionPixelSize(dimension);
+    }
+
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+    public static String deviceType(Context context) {
+        return (Utilities.isTablet(context)) ? "Tablet" : "SmartPhone";
     }
 }
