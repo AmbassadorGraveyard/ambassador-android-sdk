eddeb3bacf760dc988fa5d7bac9cc28c66069b37
jakedunahee <jake@getambassador.com>
2015-09-10 18:10:27 -0400

    [#102093184] Working on unit tests and trying different approaches

--- SDKTestApplication/AmbassadorSDK/src/test/java/AmbassadorActivityTest.java
+++ SDKTestApplication/AmbassadorSDK/src/test/java/AmbassadorActivityTest.java
@@ -1,28 +1,31 @@
 package com.example.ambassador.ambassadorsdk;
-import com.example.ambassador.ambassadorsdk.AmbassadorActivity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
-import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.exceptions.base.MockitoException;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.AdditionalMatchers.gt;
 import static org.mockito.AdditionalMatchers.lt;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
 
@@ -31,22 +34,28 @@ import static org.junit.Assert.*;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class AmbassadorActivityTest {
-    @Mock AmbassadorActivity ambassadorActivity;
+    @Mock
+    private AmbassadorActivity ambassadorActivity;
 
-    String fakeData;
+    String valueString;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
 
+
     @Test
     public void copyToClipboardTest() {
-        when(ambassadorActivity.copyShortURLToClipboard(anyString())).then(returnsFirstArg());
+        ClipboardManager mockClipboardMgr = mock(ClipboardManager.class);
+        ClipData mockClipData = mock(ClipData.class);
 
-        String stringCopied =  ambassadorActivity.copyShortURLToClipboard("test");
+        when(mockClipboardMgr.getPrimaryClip()).thenReturn(mockClipData);
 
-        assertNotNull(stringCopied);
+        mockClipboardMgr.setPrimaryClip(mockClipData);
+
+        verify(mockClipboardMgr).setPrimaryClip(mockClipData);
+        assertEquals("Expecting same clipData values", mockClipData, mockClipboardMgr.getPrimaryClip());
     }
 
     @Test
@@ -57,75 +66,101 @@ public class AmbassadorActivityTest {
         int failureClick = ambassadorActivity.respondToGridViewClick(5);
         int successClick = ambassadorActivity.respondToGridViewClick(3);
 
-        assertEquals(failureClick, 0);
-        assertEquals(successClick, 3);
+        assertEquals("Expected 0(null) and got " + failureClick, 0, failureClick);
+        assertEquals("Expected number greater than 0 and got " + successClick, 3, successClick);
     }
 
     @Test
     public void goToContactsPageTest() {
-        when(ambassadorActivity.goToContactsPage(anyBoolean())).then(returnsFirstArg());
+        Intent mockIntent = mock(Intent.class);
+        when(mockIntent.getBooleanExtra("showPhoneNumbers", false)).thenReturn(true);
 
-        String pageType = ambassadorActivity.goToContactsPage(true);
+        mockIntent.putExtra("showPhoneNumbers", true);
+        ambassadorActivity.startActivity(mockIntent);
 
-        assertEquals(pageType, "phone");
+        verify(mockIntent).putExtra("showPhoneNumbers", true);
+        verify(ambassadorActivity).startActivity(mockIntent);
+
+        assertEquals("Expected true value", true, mockIntent.getBooleanExtra("showPhoneNumbers", false));
+    }
+
+    @Test
+    public void facebookShareTest() {
+        ambassadorActivity.shareWithFacebook();
+        verify(ambassadorActivity).shareWithFacebook();
+    }
+
+    @Test
+    public void twitterShareTest() {
+        ambassadorActivity.shareWithTwitter(true);
+        ambassadorActivity.shareWithTwitter(false);
+        verify(ambassadorActivity).shareWithTwitter(true);
+        verify(ambassadorActivity).shareWithTwitter(false);
+    }
+
+    @Test
+    public void linkedInShareTest() {
+        ambassadorActivity.shareWithLinkedIn(true);
+        ambassadorActivity.shareWithLinkedIn(false);
+        verify(ambassadorActivity).shareWithLinkedIn(true);
+        verify(ambassadorActivity).shareWithLinkedIn(false);
+    }
+
+    @Test
+    public void tryAndSetURLTest() {
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                ambassadorActivity.setUrlText(anyString(), anyInt());
+                return null;
+            }
+        }).when(ambassadorActivity).tryAndSetURL(true);
+
+        ambassadorActivity.tryAndSetURL(true);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ambassadorActivity.showLoader();
+                return null;
+            }
+        }).when(ambassadorActivity).tryAndSetURL(false);
+
+        ambassadorActivity.tryAndSetURL(false);
+
+        verify(ambassadorActivity).setUrlText(anyString(), anyInt());
+        verify(ambassadorActivity).showLoader();
     }
 
     @Test
     public void setUrlTextTest() {
-        String pusherTestData = "{\"email\":\"jake@getambassador.com\"," +
-                "\"firstName\":\"erer\"," +
-                "\"lastName\":\"ere\"," +
-                "\"phoneNumber\":\"null\"," +
-                "\"urls\"" +
-                    "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
-                    "\"short_code\":\"jHjl\"," +
-                    "\"campaign_uid\":250,\"subject\":\"Check out BarderrTahwn ®!\"}," +
-                    "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
-                    "\"short_code\":\"jHjl\"," +
-                    "\"campaign_uid\":260,\"subject\":\"Check out BarderrTahwn ®!\"}," +
-                    "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
-                    "\"short_code\":\"jHjl\"," +
-                    "\"campaign_uid\":270,\"subject\":\"Check out BarderrTahwn ®!\"}," +
-                "]}";
-
-        int testCampaignID = 260;
-
-//        doAnswer(new Answer<Void>() {
-//            @Override
-//            public Void answer(InvocationOnMock invocation) throws Throwable {
-//                fakeData = "FakeDataString";
-//                return null;
-//            }
-//        }).when(ambassadorActivity).setUrlText();
-
-        ambassadorActivity.setUrlText(pusherTestData, testCampaignID);
-
-        assertEquals("FakeDataString", fakeData);
+        when(ambassadorActivity.setUrlText(anyString(), anyInt())).then(returnsFirstArg());
+        assertEquals("test", ambassadorActivity.setUrlText("test", 201));
     }
-
-    @Test (expected = JSONException.class)
-    public void urlJsonExceptionTest() throws JSONException {
-        String pusherTestData = "{\"email";
 //
-//        String pusherTestData = "{\"email\":\"jake@getambassador.com\"," +
-//                "\"firstName\":\"erer\"," +
-//                "\"lastName\":\"ere\"," +
-//                "\"phoneNumber\":\"null\"," +
-//                "\"urls\":[" +
-//                "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
-//                "\"short_code\":\"jHjl\"," +
-//                "\"campaign_uid\":250,\"subject\":\"Check out BarderrTahwn ®!\"}," +
-//                "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
-//                "\"short_code\":\"jHjl\"," +
-//                "\"campaign_uid\":260,\"subject\":\"Check out BarderrTahwn ®!\"}," +
-//                "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
-//                "\"short_code\":\"jHjl\"," +
-//                "\"campaign_uid\":270,\"subject\":\"Check out BarderrTahwn ®!\"}," +
-//                "]}";
-
-        int testCampaignID = 260;
-
-        doThrow(new JSONException(pusherTestData)).when(ambassadorActivity).setUrlText(pusherTestData, testCampaignID);
-        ambassadorActivity.setUrlText(pusherTestData, testCampaignID);
-    }
+//    @Test (expected = JSONException.class)
+//    public void urlJsonExceptionTest() throws JSONException {
+//        String pusherTestData = "{\"email";
+////
+////        String pusherTestData = "{\"email\":\"jake@getambassador.com\"," +
+////                "\"firstName\":\"erer\"," +
+////                "\"lastName\":\"ere\"," +
+////                "\"phoneNumber\":\"null\"," +
+////                "\"urls\":[" +
+////                "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
+////                "\"short_code\":\"jHjl\"," +
+////                "\"campaign_uid\":250,\"subject\":\"Check out BarderrTahwn ®!\"}," +
+////                "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
+////                "\"short_code\":\"jHjl\"," +
+////                "\"campaign_uid\":260,\"subject\":\"Check out BarderrTahwn ®!\"}," +
+////                "{\"url\":\"http://staging.mbsy.co\\/jHjl\"," +
+////                "\"short_code\":\"jHjl\"," +
+////                "\"campaign_uid\":270,\"subject\":\"Check out BarderrTahwn ®!\"}," +
+////                "]}";
+//
+//        int testCampaignID = 260;
+//
+//        doThrow(new JSONException(pusherTestData)).when(ambassadorActivity).setUrlText(pusherTestData, testCampaignID);
+//        ambassadorActivity.setUrlText(pusherTestData, testCampaignID);
+//    }
 }
