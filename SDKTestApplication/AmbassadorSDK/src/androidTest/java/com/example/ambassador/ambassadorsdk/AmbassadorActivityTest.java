cfb4039cf0af6f96a776da646cd5732981c785bb
Corey Fields <corey@getambassador.com>
2015-09-10 17:52:58 -0400

    [#102093184] began tests for contact selector refactored to make checkmark hide/show more testable

--- SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/AmbassadorActivityTest.java
+++ SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/AmbassadorActivityTest.java
@@ -25,6 +25,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
@@ -76,7 +77,19 @@ public class AmbassadorActivityTest {
 
     }
 
-    @Test
+    //@Test
+    public void testMainLayout() {
+        onView(withId(R.id.rlMainLayout)).check(matches(isDisplayed()));
+        onView(withId(R.id.tvWelcomeTitle)).check(matches(isDisplayed()));
+        onView(withId(R.id.tvWelcomeTitle)).check(matches(withText(parameters.titleText)));
+        onView(withId(R.id.tvWelcomeDesc)).check(matches(isDisplayed()));
+        onView(withId(R.id.tvWelcomeDesc)).check(matches(withText(parameters.descriptionText)));
+        onView(withId(R.id.etShortURL)).check(matches(isDisplayed()));
+        onView(withId(R.id.etShortURL)).check(matches(withText("http://staging.mbsy.co/jHjl")));
+        onView(withId(R.id.btnCopyPaste)).check(matches(isDisplayed()));
+    }
+
+    //@Test
     public void testFacebook() {
         onData(anything()).inAdapterView(withId(R.id.gvSocialGrid)).atPosition(0).perform(click());
         //onView(withText("You must")).check(matches(isDisplayed()));
@@ -89,6 +102,89 @@ public class AmbassadorActivityTest {
     }
 
     @Test
+    public void testContactsEmail() {
+        //start recording fired Intents
+        Intents.init();
+        //click email icon
+        onData(anything()).inAdapterView(withId(R.id.gvSocialGrid)).atPosition(3).perform(click());
+        //check that the Intent fired
+        intended(hasComponent(ContactSelectorActivity.class.getName()));
+        //stop recording Intents
+        Intents.release();
+
+        pressBack();
+
+        //make sure after we backed out that expected views are there
+        onView(withId(R.id.rlMainLayout)).check(matches(isDisplayed()));
+        onView(withId(R.id.gvSocialGrid)).check(matches(isDisplayed()));
+
+        onData(anything()).inAdapterView(withId(R.id.gvSocialGrid)).atPosition(3).perform(click());
+
+        onView(withId(R.id.rlMaster)).check(matches(isDisplayed()));
+        onView(withId(R.id.rlSearch)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.etSearch)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.btnDoneSearch)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.lvContacts)).check(matches(isDisplayed()));
+        onView(withId(R.id.llSendView)).check(matches(isDisplayed()));
+        onView(withId(R.id.etShareMessage)).check(matches(isDisplayed()));
+        onView(withId(R.id.etShareMessage)).check(matches(withText(containsString("http://staging.mbsy.co/jHjl"))));
+        onView(withId(R.id.btnEdit)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnDone)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.btnSend)).check(matches(not(isEnabled())));
+        onView(withId(R.id.tvNoContacts)).check(matches(not(isDisplayed())));
+
+        //test search bar
+        //COMMENTED OUT - CAN'T GET TOOLBAR TO SHOW UP IN TESTS - RESEARCH THIS
+        /*onView(withId(R.id.action_search)).perform(click());
+        onView(withId(R.id.rlSearch)).check(matches(isDisplayed()));
+        onView(withId(R.id.etSearch)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnDoneSearch)).check(matches(isDisplayed()));
+        onView(withId(R.id.action_search)).perform(click());
+        onView(withId(R.id.rlSearch)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.etSearch)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.btnDoneSearch)).check(matches(not(isDisplayed())));
+
+        //test actually filtering contacts*/
+
+
+        //test to make sure emails (not SMS) are showing
+
+        //nothing should happen when no contacts selected
+        onView(withId(R.id.btnSend)).perform(click());
+        //here test to make sure mock didn't get fired
+        onData(anything()).inAdapterView(withId(R.id.lvContacts)).atPosition(0).perform(click());
+        //onData(is(instanceOf(ContactObject.class))).inAdapterView(withId(R.id.lvContacts)).atPosition(0).perform(click());
+        //how do we check if checkmark is visible on a certain row
+        onView(withId(R.id.ivCheckMark)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnSend)).check(matches(isEnabled()));
+
+        onData(anything()).inAdapterView(withId(R.id.lvContacts)).atPosition(0).perform(click());
+        onView(withId(R.id.ivCheckMark)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.btnSend)).check(matches(not(isEnabled())));
+
+
+        //make sure edit text is disabled
+        //click pencil
+        //check edit text enabled
+        //type some text, get rid of url
+        //make sure something on contacts screen isn't showing
+        //pressback
+
+
+        //test actually sending (mock?)
+    }
+
+    //@Test
+    public void testContactsSMS() {
+
+    }
+
+    //@Test
+    public void testLinkedIn() {
+
+    }
+
+    //@Test
     public void testTwitter() {
         //clear the token
         AmbassadorSingleton.getInstance().setTwitterAccessToken(null);
@@ -104,13 +200,6 @@ public class AmbassadorActivityTest {
         pressBack();
 
         onView(withId(R.id.rlMainLayout)).check(matches(isDisplayed()));
-        onView(withId(R.id.tvWelcomeTitle)).check(matches(isDisplayed()));
-        onView(withId(R.id.tvWelcomeTitle)).check(matches(withText(parameters.titleText)));
-        onView(withId(R.id.tvWelcomeDesc)).check(matches(isDisplayed()));
-        onView(withId(R.id.tvWelcomeDesc)).check(matches(withText(parameters.descriptionText)));
-        onView(withId(R.id.etShortURL)).check(matches(isDisplayed()));
-        onView(withId(R.id.etShortURL)).check(matches(withText("http://staging.mbsy.co/jHjl")));
-        onView(withId(R.id.btnCopyPaste)).check(matches(isDisplayed()));
         onView(withId(R.id.gvSocialGrid)).check(matches(isDisplayed()));
 
         //set a token so we can test share link - this is for test embassy twitter account (developers@getambassador.com - https://twitter.com/testmbsy)
@@ -142,7 +231,6 @@ public class AmbassadorActivityTest {
         onView(withId(R.id.etTweetMessage)).perform(clearText(), closeSoftKeyboard());
         onView(withId(R.id.btnTweet)).perform(click());
 
-
         //onView(withText("INSERT LINK")).perform(click());
         //onView(withId(android.R.id.button2)).perform(click());
         //can't get the programmatically-created AlertDialog to be visible to Espresso with above two lines
@@ -158,6 +246,10 @@ public class AmbassadorActivityTest {
         //make sure message has been restored
         onView(withId(R.id.etTweetMessage)).check(matches(withText(containsString(parameters.defaultShareMessage))));
 
+        //THE BELOW CODE WORKS, BUT SINCE I COULDN'T GET MOCKING TO WORK ON TWEETREQUEST, IT WILL ACTUALLY SEND TWEETS
+        //THE VERIFY TEST PASSES, BUT IT'S COMMENTED OUT TO AVOID SPAMMING THE TWITTER
+        //WILL REVISIT ONCE WE CAN FIGURE OUT MOCKING TWEETREQUEST
+
         /*doAnswer(new Answer<Void>() {
             @Override
             public Void answer(InvocationOnMock invocation) throws Throwable {
@@ -167,13 +259,12 @@ public class AmbassadorActivityTest {
         }).when(tweetRequestMock).execute();*/
         //}).when(tweetRequestSpy).execute();
 
-
-        //type a link with a random number appended to circumvent twitter complaining about duplicate postsh
         //TweetDialog.TweetRequest tweetRequestMock = mock(TweetDialog.TweetRequest.class);
         //doNothing().when(tweetRequestSpy).execute();
         //doReturn(null).when(tweetRequestMock).execute();
         //when(tweetRequestMock.execute()).thenReturn(null);
 
+        //type a link with a random number appended to circumvent twitter complaining about duplicate postsh
         /*String tweetText = "http://www.tester.com " + _getRandomNumber();
         tweetRequestMock.tweetString = tweetText;
         onView(withId(R.id.etTweetMessage)).perform(typeText(tweetText), closeSoftKeyboard());
