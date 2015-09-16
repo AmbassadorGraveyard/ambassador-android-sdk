7b200851accb3f74c7dd8d6a737565c7e6100eda
Corey Fields <corey@getambassador.com>
2015-09-15 21:42:31 -0400

    [#102093184] forgot to stage files

--- SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/MockTweetRequestModule.java
+++ SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/MockTweetRequestModule.java
@@ -1,7 +1,26 @@
 package com.example.ambassador.ambassadorsdk;
 
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+import static org.mockito.Mockito.mock;
+
 /**
- * Created by coreyfields on 9/15/15.
+ * Created by coreyfields on 9/14/15.
  */
+@Module
 public class MockTweetRequestModule {
+    //private Context context;
+
+    public MockTweetRequestModule() {
+        //this.context = context;
+    }
+
+    @Provides @Singleton
+    TweetRequest provideTweetRequest() {
+        return mock(TweetRequest.class);
+    }
 }
