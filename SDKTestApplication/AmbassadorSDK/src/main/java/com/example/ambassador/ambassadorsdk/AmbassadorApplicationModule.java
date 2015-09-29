e13cf464d05d72c08a8f596541f478282089c392
Corey Fields <corey@getambassador.com>
2015-09-29 14:47:07 -0400

    [#103838042] renamed some files to better to conform to dagger conventions

---
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/example/ambassador/ambassadorsdk/AmbassadorApplicationModule.java
@@ -0,0 +1,52 @@
+package com.example.ambassador.ambassadorsdk;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by coreyfields on 9/14/15.
+ */
+@Module
+public class AmbassadorApplicationModule {
+    //private final Context context;
+
+/*
+    public AmbassadorApplicationModule(Context context) {
+        this.context = context;
+    }
+*/
+
+    @Provides
+    @Singleton
+    TweetRequest provideTweetRequest() {
+        return new TweetRequest();
+    }
+
+
+    @Provides
+    @Singleton
+    LinkedInRequest provideLinkedInRequest() {
+        return new LinkedInRequest();
+    }
+
+    @Provides
+    @Singleton
+    IdentifyRequest provideIdentifyRequest() {
+        return new IdentifyRequest();
+    }
+
+/*    @Provides
+    @Singleton
+    @ForActivity
+    Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    ShareDialog provideFbShareDialog() {
+        return new ShareDialog((Activity)context);
+    }*/
+}
