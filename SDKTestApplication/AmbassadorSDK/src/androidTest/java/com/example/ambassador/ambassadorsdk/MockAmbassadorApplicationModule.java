9e71ef67a45166ad905b69c59ee3ed77624c0478
Corey Fields <corey@getambassador.com>
2015-09-29 15:04:55 -0400

    [#103838042] renamed more files

--- SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/MockAmbassadorApplicationModule.java
+++ SDKTestApplication/AmbassadorSDK/src/androidTest/java/com/example/ambassador/ambassadorsdk/MockAmbassadorApplicationModule.java
@@ -12,13 +12,7 @@ import static org.mockito.Mockito.mock;
  * Created by coreyfields on 9/14/15.
  */
 @Module
-public class MockAmbassadorActivityModule {
-/*    private final Context context;
-
-    public MockAmbassadorActivityModule(Context context) {
-        this.context = context;
-    }*/
-
+public class MockAmbassadorApplicationModule {
     @Provides
     @Singleton
     TweetRequest provideTweetRequest() {
@@ -36,17 +30,4 @@ public class MockAmbassadorActivityModule {
     IdentifyRequest provideIdentifyRequest() {
         return mock(IdentifyRequest.class);
     }
-
-/*    @Provides
-    @Singleton
-    @ForActivity
-    Context provideContext() {
-        return context;
-    }
-
-    @Provides
-    @Singleton
-    ShareDialog provideFbShareDialog() {
-        return new ShareDialog((Activity)context);
-    }*/
 }
