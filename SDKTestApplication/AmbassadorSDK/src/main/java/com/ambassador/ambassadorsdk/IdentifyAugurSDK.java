082308e02a3cb43638dbbafc6725b91261504a30
Corey Fields <corey@getambassador.com>
2015-10-09 10:41:45 -0400

    [#104626508] updated code per new augur spec

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/IdentifyAugurSDK.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/IdentifyAugurSDK.java
@@ -11,18 +11,39 @@ import io.augur.wintermute.Augur;
  * Created by JakeDunahee on 9/1/15.
  */
 class IdentifyAugurSDK {
-    public String deviceID;
+    private String deviceID;
+    private String universalID;
 
     interface AugurCompletion {
         void augurComplete();
     }
 
-    public void getAugur(final AmbassadorConfig singleton, final AugurCompletion completion) {
-        Augur.getJSON(AmbassadorSingleton.get(), "***REMOVED***", new Handler.Callback() {
+    public void getAugur(final AmbassadorConfig ambassadorConfig, final AugurCompletion completion) {
+        JSONObject augurConfig = new JSONObject();
+
+        try {
+            // required
+            augurConfig.put("context", AmbassadorSingleton.get());
+            augurConfig.put("apiKey","***REMOVED***");
+            // optional
+            // config.put("endToEndEncryption", true);
+            // config.put("disableConsumerInsights", true);
+        } catch (Exception e) {
+            Utilities.debugLog("Augur", "JSON.config.error " + augurConfig.toString());
+        }
+
+        Augur.getJSON(augurConfig, new Handler.Callback() {
             @Override
             public boolean handleMessage(final Message msg) {
                 // json == the full sever response from the Augur API
                 String json = msg.getData().getString("json");
+                String error = msg.getData().getString("error");
+
+                if (error != null) {
+                    Utilities.debugLog("Augur Error", error);
+                    return true;
+                }
+
                 try {
                     // Alter the device 'type' to be "SmartPhone" or "Tablet" instead of "Android"
                     JSONObject jsonObject = new JSONObject(json);
@@ -31,12 +52,13 @@ class IdentifyAugurSDK {
                     jsonObject.put("device", device);
 
                     Utilities.debugLog("Augur", "Augur successfully received through SDK call");
-                    singleton.setIdentifyObject(jsonObject.toString());
+                    ambassadorConfig.setIdentifyObject(jsonObject.toString());
                     deviceID = Augur.DID;
+                    universalID = Augur.UID;
                     completion.augurComplete();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    json = e.toString();
+                    //json = e.toString();
                 }
 
                 return true;
@@ -46,5 +68,9 @@ class IdentifyAugurSDK {
 
     String getDeviceID() {
         return deviceID;
+    }
+
+    String getUniversalID() {
+        return universalID;
     }
 }
