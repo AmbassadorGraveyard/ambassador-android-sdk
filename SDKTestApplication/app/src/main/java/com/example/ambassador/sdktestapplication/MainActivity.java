6936b4b4f66eced1585230950c84d35b97ce2b2c
Corey Fields <corey@getambassador.com>
2015-11-18 14:11:50 -0500

    [#107087478] pass in application context to run and convert on install

--- SDKTestApplication/app/src/main/java/com/example/ambassador/sdktestapplication/MainActivity.java
+++ SDKTestApplication/app/src/main/java/com/example/ambassador/sdktestapplication/MainActivity.java
@@ -20,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         final Context context = this;
 
-        //dev
+        //dev - run
         AmbassadorSDK.runWithKeys(getApplicationContext(), "SDKToken 9de5757f801ca60916599fa3f3c92131b0e63c6a", "abfd1c89-4379-44e2-8361-ee7b87332e32");
 
-        //prod
+        //dev - run and convert on install
+        //ConversionParameters conversionParameters = new ConversionParameters();
+        //AmbassadorSDK.runWithKeysAndConvertOnInstall(getApplicationContext(), "SDKToken 9de5757f801ca60916599fa3f3c92131b0e63c6a", "abfd1c89-4379-44e2-8361-ee7b87332e32", conversionParameters);
+
+        //prod - run
         //AmbassadorSDK.runWithKeys(getApplicationContext(), "SDKToken 84444f4022a8cd4fce299114bc2e323e57e32188", "830883cd-b2a7-449c-8a3c-d1850aa8bc6b");
+
+        //prod - run and convert on install
+        //ConversionParameters conversionParameters = new ConversionParameters();
+        //AmbassadorSDK.runWithKeysAndConvertOnInstall(getApplicationContext(), "SDKToken 84444f4022a8cd4fce299114bc2e323e57e32188", "830883cd-b2a7-449c-8a3c-d1850aa8bc6b", conversionParameters);
+
 
         AmbassadorSDK.identify("jake@getambassador.com");
 
