655e3c28ee01550c17536ebdbde7f7a3ab5e5cbe
dylanmcintyre <dylan@dmcintyre.net>
2015-11-18 14:51:49 -0500

    Fixed internet stuff for receiver

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/InstallReceiver.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/InstallReceiver.java
@@ -4,12 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by dylan on 11/18/15.
@@ -22,18 +21,16 @@ public class InstallReceiver extends BroadcastReceiver {
         Toast.makeText(context, b.getString("referrer"), Toast.LENGTH_LONG).show();
 
         final String hit = "http://104.131.52.60:3000/hit/{name}".replace("{name}", b.getString("referrer"));
+
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
-                try {
-                    connection = (HttpURLConnection) new URL(hit).openConnection();
-                    connection.setDoInput(true);
-                    connection.setRequestMethod("GET");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.e("IOException", e.toString());
-                }
+                Looper.prepare();
+
+                RequestManager rm = new RequestManager();
+                HttpURLConnection connection = rm.setUpConnection("GET", hit);
+                String resp = rm.getResponse(connection, 200);
+                Log.v("RESPONSE", resp);
             }
         };
 
