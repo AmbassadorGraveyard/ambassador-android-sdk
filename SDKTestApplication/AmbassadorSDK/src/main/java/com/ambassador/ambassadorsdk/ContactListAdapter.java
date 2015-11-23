44a068e2ce92ab4a96fd3bcfee2c394694f3921e
Corey Fields <corey@getambassador.com>
2015-11-23 16:31:50 -0500

    [#107955524] unused import

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/ContactListAdapter.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/ContactListAdapter.java
@@ -12,7 +12,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
