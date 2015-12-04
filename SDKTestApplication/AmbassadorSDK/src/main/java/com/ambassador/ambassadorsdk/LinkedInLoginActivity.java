c90daa152c659edb4299f8f559a775698091f43b
dylanmcintyre <dylan@dmcintyre.net>
2015-12-04 15:41:43 -0500

    Made LinkedIn web view login better

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/LinkedInLoginActivity.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/LinkedInLoginActivity.java
@@ -2,17 +2,21 @@ package com.ambassador.ambassadorsdk;
 
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.inject.Inject;
 
@@ -21,7 +25,6 @@ import javax.inject.Inject;
  */
 public class LinkedInLoginActivity extends AppCompatActivity {
 
-    WebView webView;
     ProgressBar loader;
 
     @Inject
@@ -48,18 +51,19 @@ public class LinkedInLoginActivity extends AppCompatActivity {
             return;
         }
 
-        // UI Components
-        loader = (ProgressBar)findViewById(R.id.loadingPanel);
-        webView = (WebView)findViewById(R.id.wvSocial);
-
         _setUpToolbar();
-        loader.setVisibility(View.VISIBLE);
-        webView.setWebViewClient(new CustomBrowser());
-        webView.loadUrl("https://www.linkedin.com/uas/oauth2/authorization?" +
-                "response_type=code&client_id=777z4czm3edaef" +
-                "&redirect_uri=http://localhost:2999" +
-                "&state=987654321" +
-                "&scope=r_basicprofile%20w_share");
+
+
+        loader = (ProgressBar) findViewById(R.id.loadingPanel);
+
+        String authUrl = LinkedInApi.getAuthorizationUrl("code", "777z4czm3edaef", "http://localhost:2999", "987654321", "r_basicprofile", "w_share");
+
+        WebView webView = (WebView)findViewById(R.id.wvSocial);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        webView.setWebViewClient(new LinkedInClient());
+        webView.loadUrl(authUrl);
     }
 
     @Override
@@ -82,28 +86,21 @@ public class LinkedInLoginActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(getResources().getColor(R.color.linkedinToolBarText));
     }
 
-    private class CustomBrowser extends WebViewClient {
-
-        boolean errorOccurred = true;
+    private class LinkedInClient extends WebViewClient {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            // Breaks up url to grab separate components
-            String[] urlArray = url.split("\\?");
-            String callbackURL = urlArray[0];
-            String codeErrorString = urlArray[1];
+            return true;
+        }
 
-            // Checks for url to get code for getting Access Token
-            if (callbackURL.startsWith(AmbassadorConfig.CALLBACK_URL) && codeErrorString.startsWith("code")) {
-                String code;
-                if (url.contains("&")) {
-                    code = url.substring(url.indexOf("code=") + "code=".length(), url.indexOf("&"));
-                } else {
-                    code = url.substring(url.indexOf("code=") + "code=".length(), url.length() - 1);
-                }
-
-                errorOccurred = false;
-                requestManager.linkedInLoginRequest(code, new RequestManager.RequestCompletion() {
+        @Override
+        public void onPageFinished(final WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (url.equals(LinkedInApi.lastGeneratedAuthorizationUrl)) {
+                loader.setVisibility(View.GONE);
+            } else if (LinkedInApi.isSuccessUrl(url)) {
+                String requestToken = LinkedInApi.extractToken(url);
+                requestManager.linkedInLoginRequest(requestToken, new RequestManager.RequestCompletion() {
                     @Override
                     public void onSuccess(Object successResponse) {
                         Toast.makeText(getApplicationContext(), "Logged in successfully!", Toast.LENGTH_SHORT).show();
@@ -113,31 +110,71 @@ public class LinkedInLoginActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(Object failureResponse) {
                         Toast.makeText(getApplicationContext(), "Unable to log in, please try again!", Toast.LENGTH_SHORT).show();
+                        view.loadUrl(LinkedInApi.lastGeneratedAuthorizationUrl);
                     }
                 });
             }
+        }
+    }
 
-            view.loadUrl(url);
-            return true;
+    static class LinkedInApi {
+
+        private static final String AUTHORIZE_URL = "https://www.linkedin.com/uas/oauth2/authorization";
+        private static final String RESPONSE_TYPE_KEY = "response_type";
+        private static final String CLIENT_ID_KEY = "client_id";
+        private static final String REDIRECT_URI_KEY = "redirect_uri";
+        private static final String STATE_KEY = "state";
+        private static final String SCOPE_KEY = "scope";
+
+        public static String lastGeneratedAuthorizationUrl;
+
+        public static String getAuthorizationUrl(String responseType, String clientId, String redirectUrl, String state, String... scopes) {
+            Uri authUri = Uri.parse(AUTHORIZE_URL)
+                    .buildUpon()
+                    .appendQueryParameter(RESPONSE_TYPE_KEY, responseType)
+                    .appendQueryParameter(CLIENT_ID_KEY, clientId)
+                    .appendQueryParameter(REDIRECT_URI_KEY, "REDIRECT")
+                    .appendQueryParameter(STATE_KEY, state)
+                    .appendQueryParameter(SCOPE_KEY, getScopeValue(scopes))
+                    .build();
+
+            lastGeneratedAuthorizationUrl = authUri.toString().replace("REDIRECT", redirectUrl);
+            return lastGeneratedAuthorizationUrl;
         }
 
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-            loader.setVisibility(View.GONE);
+        private static String getScopeValue(String... scopes) {
+            String ret = "";
+            for (String scope : scopes ) {
+                ret += scope;
+                ret += " ";
+            }
+            if (ret.length() != 0) {
+                ret = ret.substring(0, ret.length() - 1);
+            }
+
+            return ret;
         }
 
-        @Override
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            super.onReceivedError(view, errorCode, description, failingUrl);
-            Log.v("ERROR", errorCode + "");
-            if (errorOccurred) {
-                Toast.makeText(LinkedInLoginActivity.this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
-                finish();
+        public static boolean isSuccessUrl(String url) {
+            try {
+                URL urlObj = new URL(url);
+                return urlObj.getHost().equals("localhost");
+            } catch (MalformedURLException e) {
+                return false;
             }
         }
 
+        public static String extractToken(String url) {
+            Uri uri = Uri.parse(url);
+            if (uri != null) {
+                return uri.getQueryParameter("code");
+            }
+
+            return null;
+        }
+
     }
+
 }
 
 
