ad10d00c6dfa3387aeb9d91d0d2512e5143364f7
dylanmcintyre <dylan@dmcintyre.net>
2015-12-07 17:48:30 -0500

    Delete traces of TweetDialog and LinkedInDialog

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/AmbassadorApplicationComponent.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/AmbassadorApplicationComponent.java
@@ -6,8 +6,6 @@ package com.ambassador.ambassadorsdk;
 public interface AmbassadorApplicationComponent {
     void inject(AmbassadorActivity ambassadorActivity);
     void inject(SocialShareDialog socialShareDialog);
-    void inject(TweetDialog tweetDialog);
-    void inject(LinkedInDialog linkedInDialog);
     void inject(LinkedInLoginActivity linkedInLoginActivity);
     void inject(ContactSelectorActivity contactSelectorActivity);
     void inject(BulkShareHelper bulkShareHelper);
