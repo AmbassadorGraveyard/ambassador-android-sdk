39b2dd1b6b1bcbc46a5d36c49a6dbac87ae4611f
Dylan McIntyre <dylan@dmcintyre.net>
2015-12-22 10:42:04 -0500

    redo AmbassadorSDK tests

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/AmbassadorSDK.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/AmbassadorSDK.java
@@ -20,27 +20,28 @@ import java.util.TimerTask;
 
 import javax.inject.Inject;
 
-/**
- * Created by JakeDunahee on 7/22/15.
- */
 public class AmbassadorSDK {
 
     @Inject
     static AmbassadorConfig ambassadorConfig;
 
     public static void presentRAF(Context context, String campaignID) {
-        Intent intent = new Intent(context, AmbassadorActivity.class);
+        Intent intent = buildIntent(context, AmbassadorActivity.class);
         ambassadorConfig.setCampaignID(campaignID);
         context.startActivity(intent);
+    }
+
+    static Intent buildIntent(Context context, Class target) {
+        return new Intent(context, target);
     }
 
     public static void identify(String emailAddress) {
         ambassadorConfig.setUserEmail(emailAddress);
 
-        IIdentify identify = new IdentifyAugurSDK();
+        IIdentify identify = buildIdentify();
         identify.getIdentity();
 
-        PusherSDK pusher = new PusherSDK();
+        PusherSDK pusher = buildPusherSDK();
         pusher.createPusher(new PusherSDK.PusherSubscribeCallback() {
             @Override
             public void pusherSubscribed() {
@@ -54,43 +55,52 @@ public class AmbassadorSDK {
         });
     }
 
+    static IIdentify buildIdentify() {
+        return new IdentifyAugurSDK();
+    }
+
+    static PusherSDK buildPusherSDK() {
+        return new PusherSDK();
+    }
+
     public static void registerConversion(ConversionParameters conversionParameters, Boolean restrictToInstall) {
         //do conversion if it's not an install conversion, or if it is, make sure that we haven't already converted on install by checking sharedprefs
         if (!restrictToInstall || !getConvertedOnInstall()) {
             Utilities.debugLog("Conversion", "restrictToInstall: " + restrictToInstall);
 
-            ConversionUtility conversionUtility = new ConversionUtility(AmbassadorSingleton.get(), conversionParameters);
+            ConversionUtility conversionUtility = buildConversionUtility(conversionParameters);
             conversionUtility.registerConversion();
         }
 
         if (restrictToInstall) setConvertedOnInstall();
     }
 
+    static ConversionUtility buildConversionUtility(ConversionParameters conversionParameters) {
+        return new ConversionUtility(AmbassadorSingleton.get(), conversionParameters);
+    }
+
+    static Boolean getConvertedOnInstall() {
+        return ambassadorConfig.getConvertedOnInstall();
+    }
+
+    static void setConvertedOnInstall() {
+        if (!ambassadorConfig.getConvertedOnInstall()) {
+            ambassadorConfig.setConvertOnInstall();
+        }
+    }
+
     public static void runWithKeys(Context context, String universalToken, String universalID) {
         AmbassadorSingleton.getInstance().init(context);
         AmbassadorSingleton.getComponent().inject(new AmbassadorSDK());
 
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("com.android.vending.INSTALL_REFERRER");
-        context.registerReceiver(InstallReceiver.getInstance(), intentFilter);
+        registerInstallReceiver(context);
 
         ambassadorConfig.setUniversalToken(universalToken);
         ambassadorConfig.setUniversalID(universalID);
         startConversionTimer();
     }
 
-
-    private static Boolean getConvertedOnInstall() {
-        return ambassadorConfig.getConvertedOnInstall();
-    }
-
-    private static void setConvertedOnInstall() {
-        if (!ambassadorConfig.getConvertedOnInstall()) {
-            ambassadorConfig.setConvertOnInstall();
-        }
-    }
-
-    private static void startConversionTimer() {
+    static void startConversionTimer() {
         final ConversionUtility utility = new ConversionUtility(AmbassadorSingleton.get());
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -99,6 +109,12 @@ public class AmbassadorSDK {
                 utility.readAndSaveDatabaseEntries();
             }
         }, 10000, 10000);
+    }
+
+    static void registerInstallReceiver(Context context) {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.android.vending.INSTALL_REFERRER");
+        context.registerReceiver(InstallReceiver.getInstance(), intentFilter);
     }
 
 }
