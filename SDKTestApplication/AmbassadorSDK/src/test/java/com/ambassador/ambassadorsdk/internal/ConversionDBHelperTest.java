644004f58b3477527e19084f2bfd513f871a0abe
Dylan McIntyre <dylan@dmcintyre.net>
2015-12-22 08:35:37 -0500

    ConversionDBHelper tests

--- SDKTestApplication/AmbassadorSDK/src/test/java/com/ambassador/ambassadorsdk/internal/ConversionDBHelperTest.java
+++ SDKTestApplication/AmbassadorSDK/src/test/java/com/ambassador/ambassadorsdk/internal/ConversionDBHelperTest.java
@@ -3,94 +3,110 @@ package com.ambassador.ambassadorsdk.internal;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import javax.inject.Singleton;
-
-import dagger.Component;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
-        Log.class,
-        ContentValues.class
+        ContentValues.class,
+        ConversionDBHelper.class,
+        Utilities.class
 })
 public class ConversionDBHelperTest {
 
-    @Singleton
-    @Component(modules = {AmbassadorApplicationModule.class})
-    public interface TestComponent {
-        void inject(ConversionDBHelperTest conversionDBHelperTest);
-    }
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        AmbassadorApplicationModule amb = new AmbassadorApplicationModule();
-        amb.setMockMode(true);
-
-        PowerMockito.mockStatic(ConversionDBHelper.class);
-
-        TestComponent component = DaggerConversionDBHelperTest_TestComponent.builder().ambassadorApplicationModule(amb).build();
-        component.inject(this);
+        PowerMockito.mockStatic(
+                Utilities.class
+        );
+        PowerMockito.spy(ConversionDBHelper.class);
     }
 
     @Test
-    public void createValuesFromConversionTest() throws Exception {
-
+    public void createValuesFromConversionTest() {
+        // ARRANGE
+        ConversionParameters conversionParameters = new ConversionParameters();
+        ContentValues contentValues = Mockito.mock(ContentValues.class);
+        Mockito.when(ConversionDBHelper.buildContentValues()).thenReturn(contentValues);
+        
+        // ACT
+        ConversionDBHelper.createValuesFromConversion(conversionParameters);
+        
+        // ASSERT
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_CAMPAIGN, conversionParameters.mbsy_campaign);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_EMAIL, conversionParameters.mbsy_email);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_FIRST_NAME, conversionParameters.mbsy_first_name);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_LAST_NAME, conversionParameters.mbsy_last_name);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_EMAIL_NEW_AMBASSADOR, conversionParameters.mbsy_email_new_ambassador);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_UID, conversionParameters.mbsy_uid);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_CUSTOM1, conversionParameters.mbsy_custom1);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_CUSTOM2, conversionParameters.mbsy_custom2);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_CUSTOM3, conversionParameters.mbsy_custom3);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_AUTO_CREATE, conversionParameters.mbsy_auto_create);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_REVENUE, conversionParameters.mbsy_revenue);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_DEACTIVATE_NEW_AMBASSADOR, conversionParameters.mbsy_deactivate_new_ambassador);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_TRANSACTION_UID, conversionParameters.mbsy_transaction_uid);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_ADD_TO_GROUP_ID, conversionParameters.mbsy_add_to_group_id);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_EVENT_DATA1, conversionParameters.mbsy_event_data1);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_EVENT_DATA2, conversionParameters.mbsy_event_data2);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_EVENT_DATA3, conversionParameters.mbsy_event_data3);
+        Mockito.verify(contentValues).put(ConversionSQLStrings.ConversionSQLEntry.MBSY_IS_APPROVED, conversionParameters.mbsy_is_approved);
     }
 
     @Test
     public void createConversionParameterWithCursorTest() {
         // ARRANGE
-        Cursor mockCursor = mock(Cursor.class);
-        when(mockCursor.getInt(anyInt())).thenReturn(0);
-        when(mockCursor.getString(anyInt())).thenReturn("");
+        ConversionParameters conversionParameters = Mockito.mock(ConversionParameters.class);
+        Mockito.when(ConversionDBHelper.buildConversionParameters()).thenReturn(conversionParameters);
+        Cursor cursor = Mockito.mock(Cursor.class);
+        Mockito.when(cursor.getInt(Mockito.anyInt())).thenReturn(-1);
+        Mockito.when(cursor.getString(Mockito.anyInt())).thenReturn("-1");
 
         // ACT
-        ConversionDBHelper.createConversionParameterWithCursor(mockCursor);
+        ConversionDBHelper.createConversionParameterWithCursor(cursor);
 
         // ASSERT
-        verify(mockCursor, times(18)).getColumnIndex(anyString());
+        Assert.assertEquals(conversionParameters.mbsy_campaign, -1);
+        Assert.assertEquals(conversionParameters.mbsy_email, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_first_name, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_last_name, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_email_new_ambassador, -1);
+        Assert.assertEquals(conversionParameters.mbsy_uid, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_custom1, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_custom2, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_custom3, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_auto_create, -1);
+        Assert.assertEquals(conversionParameters.mbsy_revenue, -1);
+        Assert.assertEquals(conversionParameters.mbsy_deactivate_new_ambassador, -1);
+        Assert.assertEquals(conversionParameters.mbsy_transaction_uid, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_add_to_group_id, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_event_data1, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_event_data2, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_event_data3, "-1");
+        Assert.assertEquals(conversionParameters.mbsy_is_approved, -1);
     }
 
     @Test
     public void deleteRowTest() {
         // ARRANGE
-        SQLiteDatabase mockDb = mock(SQLiteDatabase.class);
-        when(mockDb.delete(anyString(), anyString(), (String[]) any())).thenReturn(0);
-        String[] selectionArgs1 = { String.valueOf(5) };
-        String[] selectionArgs2 = { String.valueOf(10) };
-
-        PowerMockito.mockStatic(Log.class);
+        SQLiteDatabase db = Mockito.mock(SQLiteDatabase.class);
+        int rowId = 55;
+        Mockito.when(db.delete(Mockito.anyString(), Mockito.anyString(), Mockito.any(String[].class))).thenReturn(5);
 
         // ACT
-        ConversionDBHelper.deleteRow(mockDb, 5);
+        ConversionDBHelper.deleteRow(db, rowId);
 
         // ASSERT
-        verify(mockDb).delete(anyString(), anyString(), eq(selectionArgs1));
-
-        // ACT
-        ConversionDBHelper.deleteRow(mockDb, 10);
-
-        // ASSERT
-        verify(mockDb).delete(anyString(), anyString(), eq(selectionArgs2));
+        Mockito.verify(db).delete(ConversionSQLStrings.ConversionSQLEntry.TABLE_NAME, ConversionSQLStrings.ConversionSQLEntry._ID + " LIKE ?", new String[]{ String.valueOf(rowId) });
     }
 
 }
