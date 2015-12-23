3f7b45a48893fc4a490d3737a7998bffc230627d
Dylan McIntyre <dylan@dmcintyre.net>
2015-12-23 07:58:01 -0500

    fix boolean logic bug

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/BulkShareHelper.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/BulkShareHelper.java
@@ -84,7 +84,7 @@ public class BulkShareHelper {
         ArrayList<String> verifiedNumbers = new ArrayList<>();
         for (ContactObject contact : contactObjects) {
             String strippedNum = contact.getPhoneNumber().replaceAll("[^0-9]", "");
-            if (strippedNum.length() == 11 || strippedNum.length() == 10 || strippedNum.length() == 7 && !verifiedNumbers.contains(strippedNum)) {
+            if ((strippedNum.length() == 11 || strippedNum.length() == 10 || strippedNum.length() == 7) && !verifiedNumbers.contains(strippedNum)) {
                 verifiedNumbers.add(strippedNum);
             }
         }
