789ebabadab2676cdc7c0cfbafdae5ab64dd134c
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-11 15:12:06 -0500

    [#111238182] create helper class for handling string resources

---
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/utils/StringResource.java
@@ -0,0 +1,24 @@
+package com.ambassador.ambassadorsdk.utils;
+
+import com.ambassador.ambassadorsdk.internal.AmbassadorSingleton;
+
+public class StringResource {
+
+    private String value;
+
+    private StringResource() {}
+
+    public StringResource(int refId) {
+        this.value = AmbassadorSingleton.getInstanceContext().getString(refId);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+}
