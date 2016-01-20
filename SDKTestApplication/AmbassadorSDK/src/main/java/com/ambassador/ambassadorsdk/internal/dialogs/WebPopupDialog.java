9e8edaac3fb9b6cfea17bf08e55878a54ed5b2af
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-20 16:04:00 -0500

    [#111919797] refactor WebPopupDialog

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/dialogs/WebPopupDialog.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/dialogs/WebPopupDialog.java
@@ -1,11 +1,13 @@
 package com.ambassador.ambassadorsdk.internal.dialogs;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -22,7 +24,7 @@ import butterfork.Bind;
 import butterfork.ButterFork;
 
 /**
- *
+ * Dialog for handling external links on LinkedInLogin, opens in dialog using a WebView.
  */
 public final class WebPopupDialog extends Dialog {
 
@@ -34,15 +36,34 @@ public final class WebPopupDialog extends Dialog {
     public WebPopupDialog(Context context) {
         super(context);
 
+        if (context instanceof Activity) {
+            setOwnerActivity((Activity) context);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_webview_popup);
         ButterFork.bind(this);
 
-        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        configureDialog();
+        configureWebView();
+        enableDismissOnTouchOutside();
+    }
 
+    public void load(@NonNull String url) {
+        wvPopup.loadUrl(url);
+    }
+
+    private void configureDialog() {
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
         layoutParams.dimAmount = 0.85f;
+    }
 
+    private void configureWebView() {
         WebSettings settings = wvPopup.getSettings();
 
         settings.setAllowFileAccess(false);
@@ -55,9 +76,10 @@ public final class WebPopupDialog extends Dialog {
         wvPopup.setVerticalScrollBarEnabled(false);
         wvPopup.setHorizontalScrollBarEnabled(true);
 
-        wvPopup.setWebViewClient(new Client());
+        wvPopup.setWebViewClient(new WebClient());
+    }
 
-        /** This works for dismiss on margin click */
+    private void enableDismissOnTouchOutside() {
         getWindow().getDecorView().setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -67,12 +89,7 @@ public final class WebPopupDialog extends Dialog {
         });
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    private class Client extends WebViewClient {
+    private class WebClient extends WebViewClient {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -91,10 +108,6 @@ public final class WebPopupDialog extends Dialog {
             pbLoading.setVisibility(View.GONE);
         }
 
-    }
-
-    public void load(String url) {
-        wvPopup.loadUrl(url);
     }
 
 }
