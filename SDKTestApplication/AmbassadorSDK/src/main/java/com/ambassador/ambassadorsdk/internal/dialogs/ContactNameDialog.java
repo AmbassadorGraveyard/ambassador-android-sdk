fb35361d5e2dcd8e31a5edcd0c8ddfe86f1f4836
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-20 16:21:01 -0500

    [#111919797] ContactNameDialog refactored

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/dialogs/ContactNameDialog.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/dialogs/ContactNameDialog.java
@@ -5,9 +5,9 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.Window;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -16,6 +16,8 @@ import com.ambassador.ambassadorsdk.R;
 import com.ambassador.ambassadorsdk.internal.AmbassadorConfig;
 import com.ambassador.ambassadorsdk.internal.AmbassadorSingleton;
 import com.ambassador.ambassadorsdk.internal.RequestManager;
+import com.ambassador.ambassadorsdk.internal.utils.ColorResource;
+import com.ambassador.ambassadorsdk.internal.utils.Device;
 import com.ambassador.ambassadorsdk.internal.utils.StringResource;
 import com.ambassador.ambassadorsdk.internal.views.ShakableEditText;
 
@@ -28,21 +30,26 @@ import butterfork.Bind;
 import butterfork.ButterFork;
 
 /**
- *
+ * Dialog to ask the user for his/her name if the backend doesn't have it.
  */
 public final class ContactNameDialog extends Dialog {
 
     // region Views
     @Bind(B.id.etFirstName) protected ShakableEditText  etFirstName;
     @Bind(B.id.etLastName)  protected ShakableEditText  etLastName;
+    @Bind(B.id.btnCancel)   protected Button            btnCancel;
+    @Bind(B.id.btnContinue) protected Button            btnContinue;
     // endregion
 
     // region Dependencies
     @Inject protected RequestManager    requestManager;
     @Inject protected AmbassadorConfig  ambassadorConfig;
+    @Inject protected Device            device;
     // endregion
 
-    private ProgressDialog pd;
+    // region Local members
+    protected ProgressDialog pd;
+    // endregion
 
     public ContactNameDialog(Context context, ProgressDialog pd) {
         super(context);
@@ -51,44 +58,47 @@ public final class ContactNameDialog extends Dialog {
             setOwnerActivity((Activity) context);
         }
 
-        AmbassadorSingleton.getInstanceComponent().inject(this);
         this.pd = pd;
-        requestWindowFeature(Window.FEATURE_NO_TITLE); // Hides the default title bar
-        setContentView(R.layout.dialog_contact_name);
-        ButterFork.bind(this);
-
-        etFirstName = (ShakableEditText)findViewById(R.id.etFirstName);
-        etFirstName.setTint(context.getResources().getColor(R.color.ambassador_blue));
-        etLastName = (ShakableEditText)findViewById(R.id.etLastName);
-        etLastName.setTint(context.getResources().getColor(R.color.ambassador_blue));
-        Button btnCancel = (Button)findViewById(R.id.btnCancel);
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hide();
-            }
-        });
-
-        Button btnContinue = (Button)findViewById(R.id.btnContinue);
-        btnContinue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                continueSending();
-            }
-        });
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_contact_name);
+        ButterFork.bind(this);
+        AmbassadorSingleton.getInstanceComponent().inject(this);
+
+        setupTheme();
+        setupButtons();
     }
 
-    public void showKeyboard() {
-        InputMethodManager inputManager = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputManager.showSoftInput(etFirstName, 0);
+    private void setupTheme() {
+        etFirstName.setTint(new ColorResource(R.color.ambassador_blue).getColor());
+        etLastName.setTint(new ColorResource(R.color.ambassador_blue).getColor());
     }
 
-    void continueSending() {
+    private void setupButtons() {
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelClicked();
+            }
+        });
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                continueClicked();
+            }
+        });
+    }
+
+    private void cancelClicked() {
+        hide();
+    }
+
+    private void continueClicked() {
         if (etFirstName.getText().toString().isEmpty()) {
             Toast.makeText(getOwnerActivity(), new StringResource(R.string.first_name_empty).getValue(), Toast.LENGTH_SHORT).show();
             etFirstName.shake();
@@ -96,46 +106,42 @@ public final class ContactNameDialog extends Dialog {
             Toast.makeText(getOwnerActivity(), new StringResource(R.string.last_name_empty).getValue(), Toast.LENGTH_SHORT).show();
             etLastName.shake();
         } else {
-            handleNameInput(etFirstName.getText().toString(), etLastName.getText().toString());
+            try {
+                updateName(etFirstName.getText().toString(), etLastName.getText().toString());
+            } catch (JSONException e) {
+                dismiss();
+            }
         }
     }
 
-    void handleNameInput(final String firstName, final String lastName) {
-        //this shouldn't happen because UI enforces entry, but check anyway in case UI validation is removed
-        if (firstName == null || lastName == null) return;
+    private void updateName(@NonNull final String firstName, @NonNull final String lastName) throws JSONException {
+        JSONObject pusherData = ambassadorConfig.getPusherInfoObject();
+        if (pusherData == null) return;
 
-        JSONObject pusherData;
-        try {
-            pusherData = new JSONObject(ambassadorConfig.getPusherInfo());
-            pusherData.put("firstName", firstName);
-            pusherData.put("lastName", lastName);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return;
-        }
+        pusherData.put("firstName", firstName);
+        pusherData.put("lastName", lastName);
 
         pd.show();
         ambassadorConfig.setPusherInfo(pusherData.toString());
 
-        try {
-            requestManager.updateNameRequest(pusherData.getString("email"), firstName, lastName, new RequestManager.RequestCompletion() {
-                @Override
-                public void onSuccess(Object successResponse) {
-                    ambassadorConfig.setUserFullName(etFirstName.getText().toString(), etLastName.getText().toString());
-                    pd.dismiss();
-                    hide();
-                }
+        requestManager.updateNameRequest(pusherData.getString("email"), firstName, lastName, new RequestManager.RequestCompletion() {
+            @Override
+            public void onSuccess(Object successResponse) {
+                ambassadorConfig.setUserFullName(etFirstName.getText().toString(), etLastName.getText().toString());
+                pd.dismiss();
+                hide();
+            }
 
-                @Override
-                public void onFailure(Object failureResponse) {
-                    Toast.makeText(getOwnerActivity(), new StringResource(R.string.update_failure).getValue(), Toast.LENGTH_SHORT).show();
-                    pd.dismiss();
-                }
-            });
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+            @Override
+            public void onFailure(Object failureResponse) {
+                Toast.makeText(getOwnerActivity(), new StringResource(R.string.update_failure).getValue(), Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        });
     }
 
+    public void showKeyboard() {
+        device.openSoftKeyboard(etFirstName);
+    }
 
 }
