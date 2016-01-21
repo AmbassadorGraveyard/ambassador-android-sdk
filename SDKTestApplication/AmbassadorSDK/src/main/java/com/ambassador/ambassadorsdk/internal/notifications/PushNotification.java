e657a056e42d1a2612201f52612886cdf2f30ea7
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-21 15:17:07 -0500

    [#112154207] base push notification helper

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/notifications/PushNotification.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/notifications/PushNotification.java
@@ -1,8 +1,17 @@
 package com.ambassador.ambassadorsdk.internal.notifications;
 
+import com.ambassador.ambassadorsdk.internal.models.NotificationData;
+
 public final class PushNotification {
 
-    public PushNotification() {
+    private NotificationData notificationData;
 
+    public PushNotification(NotificationData notificationData) {
+        this.notificationData = notificationData;
     }
+
+    public void execute() {
+        // show notification
+    }
+
 }
