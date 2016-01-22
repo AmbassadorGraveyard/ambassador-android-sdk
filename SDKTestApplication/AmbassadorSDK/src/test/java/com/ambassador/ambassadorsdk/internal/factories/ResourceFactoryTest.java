d60e10448672eb7687d3ea76171c778ba38673d5
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-22 12:33:38 -0500

    [#111919797] test resource factory

---
+++ SDKTestApplication/AmbassadorSDK/src/test/java/com/ambassador/ambassadorsdk/internal/factories/ResourceFactoryTest.java
@@ -0,0 +1,63 @@
+package com.ambassador.ambassadorsdk.internal.factories;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+
+import com.ambassador.ambassadorsdk.R;
+import com.ambassador.ambassadorsdk.internal.AmbassadorSingleton;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+        AmbassadorSingleton.class,
+        ContextCompat.class
+})
+public class ResourceFactoryTest {
+
+    private Context context;
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(
+                AmbassadorSingleton.class,
+                ContextCompat.class
+        );
+
+        context = Mockito.mock(Context.class);
+        Mockito.when(AmbassadorSingleton.getInstanceContext()).thenReturn(context);
+    }
+
+    @Test
+    public void getColorTest() {
+        // ARRANGE
+        PowerMockito.when(ContextCompat.getColor(Mockito.eq(context), Mockito.eq(R.color.ambassador_blue))).thenReturn(15);
+
+        // ACT
+        int value = ResourceFactory.getColor(R.color.ambassador_blue).getColor();
+
+        // ASSERT
+        Assert.assertEquals(15, value);
+    }
+
+    @Test
+    public void getStringTest() {
+        // ARRANGE
+        Mockito.when(context.getString(R.string.post_failure)).thenReturn("ok");
+
+        // ACT
+        String value = ResourceFactory.getString(R.string.post_failure).getValue();
+
+        // ASSERT
+        Assert.assertEquals("ok", value);
+    }
+
+}
