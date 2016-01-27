cdc201be9029ec6870918feac695c3c87e2f4310
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 17:38:43 -0500

    [#112513975] extend application, implement singleton, init AmbassadorSDK

---
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/Demo.java
@@ -0,0 +1,29 @@
+package com.ambassador.demoapp;
+
+import android.app.Application;
+
+import com.ambassador.ambassadorsdk.AmbassadorSDK;
+
+public final class Demo extends Application {
+
+    private static final boolean IS_RELEASE = false;
+
+    private static Demo instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Demo.instance = this;
+
+        if (IS_RELEASE) {
+            AmbassadorSDK.runWithKeys(getApplicationContext(), "SDKToken ***REMOVED***", "***REMOVED***");
+        } else {
+            AmbassadorSDK.runWithKeys(this, "SDKToken ***REMOVED***", "***REMOVED***");
+        }
+    }
+
+    public static Demo get() {
+        return instance;
+    }
+
+}
