7b0ce08927f45fc1130a3c8aafd1301e924aba9f
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 15:43:47 -0500

    [#112513975] signup bind views and set button on click

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/fragments/SignupFragment.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/fragments/SignupFragment.java
@@ -6,17 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ambassador.demoapp.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public final class SignupFragment extends Fragment {
+
+    @Bind(R.id.etEmail)     protected EditText  etEmail;
+    @Bind(R.id.etUsername)  protected EditText  etUsername;
+    @Bind(R.id.etPassword)  protected EditText  etPassword;
+    @Bind(R.id.btnSignup)    protected Button   btnSignup;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        ButterKnife.bind(this, view);
+
+        btnSignup.setOnClickListener(btnLoginOnClickListener);
 
         return view;
     }
 
+    protected View.OnClickListener btnLoginOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), "hit", Toast.LENGTH_LONG).show();
+        }
+    };
 }
