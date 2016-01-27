2d2e329c00b148897344ab6fa7d31ebbb95c89dd
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 16:50:17 -0500

    [#112513975] tab icons + titles

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/MainActivity.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.ambassador.demoapp;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,8 +13,12 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ambassador.demoapp.fragments.LoginFragment;
 import com.ambassador.demoapp.fragments.ReferFragment;
@@ -33,8 +39,16 @@ public final class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        vpPages.setAdapter(new TabFragmentPagerAdapter(getSupportFragmentManager()));
+        TabFragmentPagerAdapter adapter = new TabFragmentPagerAdapter(getSupportFragmentManager());
+        vpPages.setAdapter(adapter);
         tlTabs.setupWithViewPager(vpPages);
+
+        for (int i = 0; i < tlTabs.getTabCount(); i++) {
+            TabLayout.Tab tab = tlTabs.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(adapter.getTabView(i));
+            }
+        }
 
         setTitle(Html.fromHtml("<small>Ambassador Demo</small>"));
         ActionBar actionBar = getSupportActionBar();
@@ -67,10 +81,10 @@ public final class MainActivity extends AppCompatActivity {
         public TabFragmentPagerAdapter(FragmentManager fm) {
             super(fm);
             tabs = new TabModel[4];
-            tabs[0] = new TabModel("Login", new LoginFragment());
-            tabs[1] = new TabModel("Sign Up", new SignupFragment());
-            tabs[2] = new TabModel("Buy Now", new StoreFragment());
-            tabs[3] = new TabModel("Refer a Friend", new ReferFragment());
+            tabs[0] = new TabModel("Login", R.drawable.ic_login, new LoginFragment());
+            tabs[1] = new TabModel("Sign Up", R.drawable.ic_signup, new SignupFragment());
+            tabs[2] = new TabModel("Buy Now", R.drawable.ic_buy, new StoreFragment());
+            tabs[3] = new TabModel("Refer a Friend", R.drawable.ic_raf, new ReferFragment());
         }
 
         @Override
@@ -83,23 +97,37 @@ public final class MainActivity extends AppCompatActivity {
             return tabs[position].getFragment();
         }
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return tabs[position].getTitle();
+        @NonNull
+        public View getTabView(int position) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab, null);
+
+            ImageView ivIcon = (ImageView) view.findViewById(R.id.ivIcon);
+            ivIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, tabs[position].getDrawableId()));
+
+            TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+            tvTitle.setText(tabs[position].getTitle());
+
+            return view;
         }
 
         private final class TabModel {
 
             private String title;
+            private int drawableId;
             private Fragment fragment;
 
-            public TabModel(String title, Fragment fragment) {
+            public TabModel(String title, @DrawableRes int drawableId, Fragment fragment) {
                 this.title = title;
+                this.drawableId = drawableId;
                 this.fragment = fragment;
             }
 
             public String getTitle() {
                 return title;
+            }
+
+            public int getDrawableId() {
+                return drawableId;
             }
 
             public Fragment getFragment() {
