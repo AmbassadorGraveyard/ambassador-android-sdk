97227e6cac69221b77794be87ae217b1a8991a75
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 14:07:50 -0500

    [#112513975] basic fragment inflation

--- SDKTestApplication/app/src/main/java/com/example/ambassador/sdktestapplication/fragments/SignupFragment.java
+++ SDKTestApplication/app/src/main/java/com/example/ambassador/sdktestapplication/fragments/SignupFragment.java
@@ -1,9 +1,22 @@
 package com.example.ambassador.sdktestapplication.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ambassador.sdktestapplication.R;
 
 public final class SignupFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
 
+        return view;
+    }
 
 }
