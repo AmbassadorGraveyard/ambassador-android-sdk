b1aa464ff01adbdd52ec7dd6a634d55c8fcb1ac4
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 15:33:44 -0500

    [#112513975] bind views and button on click

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/fragments/LoginFragment.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/fragments/LoginFragment.java
@@ -6,17 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ambassador.demoapp.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public final class LoginFragment extends Fragment {
+
+    @Bind(R.id.etEmail)     protected EditText  etEmail;
+    @Bind(R.id.etPassword)  protected EditText  etPassword;
+    @Bind(R.id.btnLogin)    protected Button    btnLogin;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, view);
+
+        btnLogin.setOnClickListener(btnLoginOnClickListener);
 
         return view;
     }
+
+    protected View.OnClickListener btnLoginOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), "hit", Toast.LENGTH_LONG).show();
+        }
+    };
 
 }
