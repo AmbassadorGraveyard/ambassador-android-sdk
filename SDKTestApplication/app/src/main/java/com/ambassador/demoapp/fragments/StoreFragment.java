9c52c2dea83401fce0fa9c59431f3ccf755d2f1d
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 21:09:02 -0500

    [#112513975] send them to login if not identified

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/fragments/StoreFragment.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/fragments/StoreFragment.java
@@ -1,5 +1,6 @@
 package com.ambassador.demoapp.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.ambassador.demoapp.Demo;
+import com.ambassador.demoapp.MainActivity;
 import com.ambassador.demoapp.R;
 
 import butterknife.Bind;
@@ -32,11 +35,26 @@ public final class StoreFragment extends Fragment {
     protected View.OnClickListener btnBuyOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            new AlertDialog.Builder(getActivity())
-                    .setTitle("Purchase successful")
-                    .setMessage("Thank you for buying from Ambassador!")
-                    .setPositiveButton("Done", null)
-                    .show();
+            if (Demo.get().getEmail() != null) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Purchase successful")
+                        .setMessage("Thank you for buying from Ambassador!")
+                        .setPositiveButton("Done", null)
+                        .show();
+
+                Demo.get().conversion();
+            } else {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Authentication needed")
+                        .setMessage("Please login to complete your purchase.")
+                        .setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ((MainActivity) getActivity()).switchToTabAtIndex(0);
+                            }
+                        }).setNegativeButton("Cancel", null)
+                        .show();
+            }
         }
     };
 
