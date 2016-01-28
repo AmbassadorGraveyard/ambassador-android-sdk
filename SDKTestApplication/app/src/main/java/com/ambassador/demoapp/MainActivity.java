80dde2e4a451bf13cfcede3c7fd00c16324a2c76
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-28 09:26:33 -0500

    [#112513975] remove settings button in action bar

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/MainActivity.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/MainActivity.java
@@ -14,8 +14,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -55,23 +53,6 @@ public final class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.actionBarColor)));
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void switchToTabAtIndex(int position) {
