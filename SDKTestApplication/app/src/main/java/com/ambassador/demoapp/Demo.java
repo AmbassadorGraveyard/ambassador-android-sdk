bb577971641fc1805efd3b39c29d96935687b534
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-27 21:09:54 -0500

    [#112513975] ambassador idk interfacing

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/Demo.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/Demo.java
@@ -1,8 +1,12 @@
 package com.ambassador.demoapp;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.ambassador.ambassadorsdk.AmbassadorSDK;
+import com.ambassador.ambassadorsdk.ConversionParameters;
 
 public final class Demo extends Application {
 
@@ -10,10 +14,14 @@ public final class Demo extends Application {
 
     private static Demo instance;
 
+    private SharedPreferences prefs;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Demo.instance = this;
+
+        prefs = getSharedPreferences("amb_demo", MODE_PRIVATE);
 
         if (IS_RELEASE) {
             AmbassadorSDK.runWithKeys(getApplicationContext(), "SDKToken 84444f4022a8cd4fce299114bc2e323e57e32188", "830883cd-b2a7-449c-8a3c-d1850aa8bc6b");
@@ -24,6 +32,40 @@ public final class Demo extends Application {
 
     public void identify(String email) {
         AmbassadorSDK.identify(email);
+    }
+
+    public void conversion() {
+        String email = getEmail();
+        if (email == null) return;
+        AmbassadorSDK.registerConversion(
+                new ConversionParameters.Builder()
+                        .setEmail(email)
+                        .setCampaign(260)
+                        .setRevenue(24)
+                        .setCustom1("This is a conversion from the Ambassador SDK Android test application.")
+                        .setCustom2("Buy conversion registered for $24.00")
+                        .build()
+                , false
+        );
+
+    }
+
+    public void setEmail(String email) {
+        prefs.edit().putString("email", email).apply();
+    }
+
+    @Nullable
+    public String getEmail() {
+        return prefs.getString("email", null);
+    }
+
+    public void setCampaignId(String campaignId) {
+        prefs.edit().putString("campaignId", campaignId).apply();
+    }
+
+    @NonNull
+    public String getCampaignId() {
+        return prefs.getString("campaignId", "260");
     }
 
     public static Demo get() {
