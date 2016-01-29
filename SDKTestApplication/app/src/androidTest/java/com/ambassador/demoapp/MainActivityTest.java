84be0a5015ad1660d51e5971211c0df5a7752d5a
Dylan McIntyre <dylan@dmcintyre.net>
2016-01-29 10:33:49 -0500

    [#112636043] type into fields for login

--- SDKTestApplication/app/src/androidTest/java/com/ambassador/demoapp/MainActivityTest.java
+++ SDKTestApplication/app/src/androidTest/java/com/ambassador/demoapp/MainActivityTest.java
@@ -7,6 +7,8 @@ import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
 
 import com.ambassador.demoapp.MainActivity;
@@ -37,14 +39,29 @@ public class MainActivityTest {
 
         Context context = InstrumentationRegistry.getContext();
         Intent intent  = context.getPackageManager().getLaunchIntentForPackage(PACKAGE_NAME);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
 
         device.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)), LAUNCH_TIMEOUT);
     }
 
     @Test
-    public void test() {
+    public void loginFilledInputsDoesIdentifyTest() throws Exception {
+        // ARRANGE
+        UiObject usernameField = device.findObject(new UiSelector().description("usernameField").className("android.widget.EditText"));
+        UiObject passwordField = device.findObject(new UiSelector().description("passwordField").className("android.widget.EditText"));
+        UiObject loginButton = device.findObject(new UiSelector().description("loginButton").className("android.widget.Button"));
 
+        // ACT
+        usernameField.click();
+        usernameField.setText("jake@getambassador.com");
+
+        passwordField.click();
+        passwordField.setText("password");
+
+        loginButton.click();
+
+        // ASSERT
     }
 
 }
