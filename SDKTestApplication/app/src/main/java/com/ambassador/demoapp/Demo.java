be915c91eee386f48985bee470bda8da918e7aac
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-01 10:57:52 -0500

    [#112636043] store local conversion info demo app

--- SDKTestApplication/app/src/main/java/com/ambassador/demoapp/Demo.java
+++ SDKTestApplication/app/src/main/java/com/ambassador/demoapp/Demo.java
@@ -8,6 +8,9 @@ import android.support.annotation.Nullable;
 import com.ambassador.ambassadorsdk.AmbassadorSDK;
 import com.ambassador.ambassadorsdk.ConversionParameters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Demo extends Application {
 
     private static final boolean IS_RELEASE = false;
@@ -15,6 +18,7 @@ public final class Demo extends Application {
     private static Demo instance;
 
     private SharedPreferences prefs;
+    private List<ConversionParameters> conversions;
 
     @Override
     public void onCreate() {
@@ -28,38 +32,42 @@ public final class Demo extends Application {
         } else {
             AmbassadorSDK.runWithKeys(this, "SDKToken ***REMOVED***", "***REMOVED***");
         }
+
+        conversions = new ArrayList<>();
     }
 
     public void identify(String email) {
         Demo.get().setEmail(email);
+        conversions = new ArrayList<>();
         AmbassadorSDK.identify(email);
     }
 
     public void signupConversion(@NonNull String email, @NonNull String username) {
-        AmbassadorSDK.registerConversion(
-                new ConversionParameters.Builder()
-                        .setEmail(email)
-                        .setCampaign(Integer.parseInt(getCampaignId()))
-                        .setCustom1("This is a buyConversion from the Ambassador SDK Android test application.")
-                        .setCustom2("Username registered: " + username)
-                        .build()
-                , true
-        );
+        ConversionParameters parameters = new ConversionParameters.Builder()
+                .setEmail(email)
+                .setCampaign(Integer.parseInt(getCampaignId()))
+                .setCustom1("This is a buyConversion from the Ambassador SDK Android test application.")
+                .setCustom2("Username registered: " + username)
+                .build();
+
+        AmbassadorSDK.registerConversion(parameters, true);
+        conversions.add(parameters);
     }
 
     public void buyConversion() {
         String email = getEmail();
         if (email == null) return;
-        AmbassadorSDK.registerConversion(
-                new ConversionParameters.Builder()
-                        .setEmail(email)
-                        .setCampaign(Integer.parseInt(getCampaignId()))
-                        .setRevenue(24)
-                        .setCustom1("This is a buyConversion from the Ambassador SDK Android test application.")
-                        .setCustom2("Buy buyConversion registered for $24.00")
-                        .build()
-                , false
-        );
+
+        ConversionParameters parameters = new ConversionParameters.Builder()
+                .setEmail(email)
+                .setCampaign(Integer.parseInt(getCampaignId()))
+                .setRevenue(24)
+                .setCustom1("This is a buyConversion from the Ambassador SDK Android test application.")
+                .setCustom2("Buy buyConversion registered for $24.00")
+                .build();
+
+        AmbassadorSDK.registerConversion(parameters, false);
+        conversions.add(parameters);
     }
 
     public void presentRAF(String path) {
