1e841324b0db0ed81468e1898d9a2fd0c623134b
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-04 15:24:38 -0500

    [#111333462] configure pro guard obfuscation

--- SDKTestApplication/app/src/androidTest/java/com/ambassador/demoapp/MainActivityTest.java
+++ SDKTestApplication/app/src/androidTest/java/com/ambassador/demoapp/MainActivityTest.java
@@ -4,6 +4,9 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import com.ambassador.demoapp.MainActivity;
+import com.ambassador.demoapp.R;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,26 +14,9 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 
-@RunWith(AndroidJUnit4.class)
-@LargeTest
 public class MainActivityTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
-
-    @Test
-    public void testPresentRaf() {
-        //onView(withId(R.id.btnPresentRAF1)).perform(click());
-        onView(withId(R.id.llParent)).check(matches(isDisplayed()));
-        onView(withId(R.id.tvWelcomeTitle)).check(matches(isDisplayed()));
-        onView(withId(R.id.tvWelcomeDesc)).check(matches(isDisplayed()));
-        onView(withId(R.id.etShortURL)).check(matches(isDisplayed()));
-        onView(withId(R.id.etShortURL)).check(matches(not(withText(""))));
-        onView(withId(R.id.btnCopy)).check(matches(isDisplayed()));
-        onView(withId(R.id.gvSocialGrid)).check(matches(isDisplayed()));
-    }
 }
