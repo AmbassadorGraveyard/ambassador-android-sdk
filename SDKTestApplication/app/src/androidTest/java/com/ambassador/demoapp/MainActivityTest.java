7154f53ed54f7f9d9684ff75e13a3855835f80b9
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-04 16:01:51 -0500

    remove unused imports

--- SDKTestApplication/app/src/androidTest/java/com/ambassador/demoapp/MainActivityTest.java
+++ SDKTestApplication/app/src/androidTest/java/com/ambassador/demoapp/MainActivityTest.java
@@ -1,22 +1,5 @@
 package com.example.ambassador.sdktestapplication;
 
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import com.ambassador.demoapp.MainActivity;
-import com.ambassador.demoapp.R;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
-
 public class MainActivityTest {
 
 }
