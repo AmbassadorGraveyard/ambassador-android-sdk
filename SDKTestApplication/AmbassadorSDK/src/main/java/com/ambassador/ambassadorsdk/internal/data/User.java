39306e4ddd85733fa56fe6a7187e9cd275b249ae
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-08 15:08:06 -0500

    [#113123483] implement save and clear on User

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/data/User.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/data/User.java
@@ -1,7 +1,17 @@
 package com.ambassador.ambassadorsdk.internal.data;
 
-import com.ambassador.ambassadorsdk.internal.models.AugurData;
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import com.ambassador.ambassadorsdk.internal.AmbassadorSingleton;
+import com.ambassador.ambassadorsdk.internal.models.AugurData;
+import com.google.gson.Gson;
+
+
+/**
+ * Stores, serializes and unserializes data pertaining to an Ambassador identified user,
+ * and what the backend needs to register conversions and track shares.
+ */
 public class User {
 
     protected String firstName;
@@ -10,5 +20,27 @@ public class User {
     protected String gcmToken;
     protected AugurData augurData;
 
+    /**
+     * Serializes data into a JSON string and saves in SharedPreferences,
+     * keyed on the user's email.
+     */
+    public void save() {
+        if (AmbassadorSingleton.getInstanceContext() != null) {
+            String data = new Gson().toJson(this);
+            SharedPreferences sharedPreferences = AmbassadorSingleton.getInstanceContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+            sharedPreferences.edit().putString(email, data).apply();
+        }
+    }
+
+    /**
+     * Clears instance data from this object's fields.
+     */
+    public void clear() {
+        firstName = null;
+        lastName = null;
+        email = null;
+        gcmToken = null;
+        augurData = null;
+    }
 
 }
