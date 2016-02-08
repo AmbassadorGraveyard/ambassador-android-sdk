725a7862ba09a1e8c4512ee5a7fd06ab1cf10151
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-08 14:57:06 -0500

    [#113123483] add Campaign fields

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/data/Campaign.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/data/Campaign.java
@@ -1,4 +1,11 @@
 package com.ambassador.ambassadorsdk.internal.data;
 
 public class Campaign {
+
+    protected String id;
+    protected String shortCode;
+    protected String shareMessage;
+    protected String emailSubject;
+    protected String referredByShortCode;
+
 }
