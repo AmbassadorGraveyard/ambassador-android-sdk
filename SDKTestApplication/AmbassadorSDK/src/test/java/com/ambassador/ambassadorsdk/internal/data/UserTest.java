364eb2f3598a0caa0579362e821b63683c3a7cf4
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-08 17:39:49 -0500

    [#113123483] User tests

---
+++ SDKTestApplication/AmbassadorSDK/src/test/java/com/ambassador/ambassadorsdk/internal/data/UserTest.java
@@ -0,0 +1,124 @@
+package com.ambassador.ambassadorsdk.internal.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.ambassador.ambassadorsdk.internal.AmbassadorSingleton;
+import com.ambassador.ambassadorsdk.internal.models.AugurData;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+        AmbassadorSingleton.class,
+})
+public class UserTest {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(
+                AmbassadorSingleton.class
+        );
+
+        context = Mockito.mock(Context.class);
+        Mockito.when(AmbassadorSingleton.getInstanceContext()).thenReturn(context);
+
+        sharedPreferences = Mockito.mock(SharedPreferences.class);
+        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(sharedPreferences);
+
+        editor = Mockito.mock(SharedPreferences.Editor.class);
+        Mockito.when(sharedPreferences.edit()).thenReturn(editor);
+        Mockito.when(editor.putString(Mockito.anyString(), Mockito.anyString())).thenReturn(editor);
+    }
+
+    @Test
+    public void save_withNonNullContext_doesSaveSerialized() {
+        // ARRANGE
+        User user = new User();
+        user.email = "jake@getambassador.com";
+        user.firstName = "jake";
+        user.lastName = "dunahee";
+        user.gcmToken = "gcmToken";
+        user.augurData = null;
+
+        // ACT
+        user.save();
+
+        // ASSERT
+        Mockito.verify(context).getSharedPreferences(Mockito.eq("user"), Mockito.eq(Context.MODE_PRIVATE));
+        Mockito.verify(editor).putString(Mockito.eq("jake@getambassador.com"), Mockito.eq("{\"firstName\":\"jake\",\"lastName\":\"dunahee\",\"email\":\"jake@getambassador.com\",\"gcmToken\":\"gcmToken\"}"));
+        Mockito.verify(editor).apply();
+    }
+
+    @Test
+    public void save_withNullContext_doesNotSave() {
+        // ARRANGE
+        Mockito.when(AmbassadorSingleton.getInstanceContext()).thenReturn(null);
+
+        User user = new User();
+        user.email = "jake@getambassador.com";
+        user.firstName = "jake";
+        user.lastName = "dunahee";
+        user.gcmToken = "gcmToken";
+        user.augurData = null;
+
+        // ACT
+        user.save();
+
+        // ASSERT
+        Mockito.verify(context, Mockito.never()).getSharedPreferences(Mockito.eq("user"), Mockito.eq(Context.MODE_PRIVATE));
+        Mockito.verify(sharedPreferences, Mockito.never()).edit();
+        Mockito.verify(editor, Mockito.never()).putString(Mockito.anyString(), Mockito.anyString());
+        Mockito.verify(editor, Mockito.never()).apply();
+    }
+
+    @Test
+    public void clear_doesClearAllFields() {
+        // ARRANGE
+        User user = new User();
+        user.email = "jake@getambassador.com";
+        user.firstName = "jake";
+        user.lastName = "dunahee";
+        user.gcmToken = "gcmToken";
+        user.augurData = new AugurData();
+
+        // ACT
+        user.clear();
+
+        // ASSERT
+        Assert.assertNull(user.getEmail());
+        Assert.assertNull(user.getFirstName());
+        Assert.assertNull(user.getLastName());
+        Assert.assertNull(user.getGcmToken());
+        Assert.assertNull(user.getAugurData());
+    }
+
+    @Test
+    public void setters_doSaveOnInvocation() {
+        // ARRANGE
+        User user = Mockito.spy(new User());
+        Mockito.doNothing().when(user).save();
+
+        // ACT
+        user.setEmail("jake@getambassador.com");
+        user.setFirstName("firstName");
+        user.setLastName("lastName");
+        user.setGcmToken("gcmToken");
+        user.setAugurData(new AugurData());
+
+        // ASSERT
+        Mockito.verify(user, Mockito.times(5)).save();
+    }
+
+}
