1d25737c9e40a21fda4761c5d2d1ac4b98b3fe03
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-08 15:00:16 -0500

    [#113123483] add User fields

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/data/User.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/data/User.java
@@ -1,4 +1,14 @@
 package com.ambassador.ambassadorsdk.internal.data;
 
+import com.ambassador.ambassadorsdk.internal.models.AugurData;
+
 public class User {
+
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String gcmToken;
+    protected AugurData augurData;
+
+
 }
