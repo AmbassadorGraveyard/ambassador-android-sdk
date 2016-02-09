31e89669c2e92092afa6cab9304f4d1159171d60
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-09 16:21:58 -0500

    [#113123483] fix unit tests

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/AmbassadorConfig.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/AmbassadorConfig.java
@@ -3,5 +3,5 @@ package com.ambassador.ambassadorsdk.internal;
 public class AmbassadorConfig { // TODO: Make final after UI tests figured out
 
     public static final Boolean isReleaseBuild = false;
-    
+
 }
