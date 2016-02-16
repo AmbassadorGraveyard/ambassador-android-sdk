b175b8c787ae661b4e85da30fcb2e85ab821adc0
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-16 17:33:15 -0500

    use app icon

--- SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/notifications/PushNotification.java
+++ SDKTestApplication/AmbassadorSDK/src/main/java/com/ambassador/ambassadorsdk/internal/notifications/PushNotification.java
@@ -31,13 +31,12 @@ public final class PushNotification {
         PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, result, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Notification notification = new NotificationCompat.Builder(context)
-                .setSmallIcon(android.R.drawable.ic_dialog_map)
+                .setSmallIcon(context.getApplicationInfo().icon)
                 .setContentTitle("AmbassadorSDK")
                 .setContentText("This is a push notification")
                 .setContentIntent(resultPendingIntent)
                 .setAutoCancel(true)
                 .build();
-
 
         int notificationId = 001;
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
