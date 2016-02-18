de889336080bd9ddbcbc63627fe9246b75a88927
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-18 16:15:25 -0500

    import Log

--- ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/api/PusherManager.java
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/api/PusherManager.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 import com.ambassador.ambassadorsdk.BuildConfig;
 import com.ambassador.ambassadorsdk.R;
