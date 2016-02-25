61d7b56fffbff31c56d18ae2b3f92eb301583b91
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-25 09:57:28 -0500

    convert welcomescreendata to builder model

--- ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/models/WelcomeScreenData.java
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/models/WelcomeScreenData.java
@@ -10,13 +10,10 @@ public class WelcomeScreenData {
     protected String imageUrl;
     protected String title;
     protected String message;
-
     protected String buttonText;
     protected View.OnClickListener buttonOnClickListener;
-
     protected String link1Text;
     protected View.OnClickListener link1OnClickListener;
-
     protected String link2Text;
     protected View.OnClickListener link2OnClickListener;
 
@@ -24,72 +21,95 @@ public class WelcomeScreenData {
         return imageUrl;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
     public String getButtonText() {
         return buttonText;
-    }
-
-    public void setButtonText(String buttonText) {
-        this.buttonText = buttonText;
     }
 
     public View.OnClickListener getButtonOnClickListener() {
         return buttonOnClickListener;
     }
 
-    public void setButtonOnClickListener(View.OnClickListener buttonOnClickListener) {
-        this.buttonOnClickListener = buttonOnClickListener;
-    }
-
     public String getLink1Text() {
         return link1Text;
-    }
-
-    public void setLink1Text(String link1Text) {
-        this.link1Text = link1Text;
     }
 
     public View.OnClickListener getLink1OnClickListener() {
         return link1OnClickListener;
     }
 
-    public void setLink1OnClickListener(View.OnClickListener link1OnClickListener) {
-        this.link1OnClickListener = link1OnClickListener;
-    }
-
     public String getLink2Text() {
         return link2Text;
-    }
-
-    public void setLink2Text(String link2Text) {
-        this.link2Text = link2Text;
     }
 
     public View.OnClickListener getLink2OnClickListener() {
         return link2OnClickListener;
     }
 
-    public void setLink2OnClickListener(View.OnClickListener link2OnClickListener) {
-        this.link2OnClickListener = link2OnClickListener;
+    public static class Builder {
+
+        protected WelcomeScreenData welcomeScreenData;
+
+        public Builder() {
+            welcomeScreenData = new WelcomeScreenData();
+        }
+
+        public Builder setImageUrl(String imageUrl) {
+            welcomeScreenData.imageUrl = imageUrl;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            welcomeScreenData.title = title;
+            return this;
+        }
+
+        public Builder setMessage(String message) {
+            welcomeScreenData.message = message;
+            return this;
+        }
+
+        public Builder setButtonText(String buttonText) {
+            welcomeScreenData.buttonText = buttonText;
+            return this;
+        }
+
+        public Builder setButtonOnClickListener(View.OnClickListener buttonOnClickListener) {
+            welcomeScreenData.buttonOnClickListener = buttonOnClickListener;
+            return this;
+        }
+
+        public Builder setLink1Text(String link1Text) {
+            welcomeScreenData.link1Text = link1Text;
+            return this;
+        }
+
+        public Builder setLink1OnClickListener(View.OnClickListener link1OnClickListener) {
+            welcomeScreenData.link1OnClickListener = link1OnClickListener;
+            return this;
+        }
+
+        public Builder setLink2Text(String link2Text) {
+            welcomeScreenData.link2Text = link2Text;
+            return this;
+        }
+
+        public Builder setLink2OnClickListener(View.OnClickListener link2OnClickListener) {
+            welcomeScreenData.link2OnClickListener = link2OnClickListener;
+            return this;
+        }
+
+        public WelcomeScreenData build() {
+            return welcomeScreenData;
+        }
+        
     }
 
 }
