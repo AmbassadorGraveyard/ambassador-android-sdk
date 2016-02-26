b405cf1299ad355defdefb4aad0fef35f4c5dbb6
Dylan McIntyre <dylan@dmcintyre.net>
2016-02-26 11:25:18 -0500

    [#114127453] horizontal welcome screen mvp

---
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/views/AutoResizeTextView.java
@@ -0,0 +1,204 @@
+package com.ambassador.ambassadorsdk.internal.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.TextView;
+
+/**
+ * https://github.com/AndroidDeveloperLB/AutoFitTextView/blob/master/AutoFitTextViewLibrary/src/com/lb/auto_fit_textview/AutoResizeTextView.java
+ * Taken on (02/26/2016) ddd35ca.
+ */
+public class AutoResizeTextView extends TextView {
+
+    private static final int NO_LINE_LIMIT = -1;
+    private final RectF availableSpaceRect = new RectF();
+    private final SizeTester sizeTester;
+    private float maxTextSize;
+    private float spacingMult = 1.0f;
+    private float spacingAdd = 0.0f;
+    private float minTextSize;
+    private int widthLimit, maxLines;
+    private boolean initialized = false;
+    private TextPaint paint;
+
+    private interface SizeTester {
+        int onTestSize(int suggestedSize, RectF availableSpace);
+    }
+
+    public AutoResizeTextView(final Context context) {
+        this(context, null, 0);
+    }
+
+    public AutoResizeTextView(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AutoResizeTextView(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
+        minTextSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics());
+        maxTextSize = getTextSize();
+        paint = new TextPaint(getPaint());
+        if (maxLines == 0)
+            maxLines = NO_LINE_LIMIT;
+        sizeTester = new SizeTester() {
+            final RectF textRect = new RectF();
+
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public int onTestSize(final int suggestedSize, final RectF availableSPace) {
+                paint.setTextSize(suggestedSize);
+                final String text = getText().toString();
+                final boolean singleLine = getMaxLines() == 1;
+                if (singleLine) {
+                    textRect.bottom = paint.getFontSpacing();
+                    textRect.right = paint.measureText(text);
+                } else {
+                    final StaticLayout layout = new StaticLayout(text, paint, widthLimit, Layout.Alignment.ALIGN_NORMAL, spacingMult, spacingAdd, true);
+                    if (getMaxLines() != NO_LINE_LIMIT && layout.getLineCount() > getMaxLines())
+                        return 1;
+                    textRect.bottom = layout.getHeight();
+                    int maxWidth = -1;
+                    for (int i = 0; i < layout.getLineCount(); i++)
+                        if (maxWidth < layout.getLineRight(i) - layout.getLineLeft(i))
+                            maxWidth = (int) layout.getLineRight(i) - (int) layout.getLineLeft(i);
+                    textRect.right = maxWidth;
+                }
+                textRect.offsetTo(0, 0);
+                if (availableSPace.contains(textRect))
+                    return -1;
+                return 1;
+            }
+        };
+        initialized = true;
+    }
+
+    @Override
+    public void setTypeface(final Typeface tf) {
+        super.setTypeface(tf);
+        adjustTextSize();
+    }
+
+    @Override
+    public void setTextSize(final float size) {
+        maxTextSize = size;
+        adjustTextSize();
+    }
+
+    @Override
+    public void setMaxLines(final int maxlines) {
+        super.setMaxLines(maxlines);
+        maxLines = maxlines;
+        adjustTextSize();
+    }
+
+    @Override
+    public int getMaxLines() {
+        return maxLines;
+    }
+
+    @Override
+    public void setSingleLine() {
+        super.setSingleLine();
+        maxLines = 1;
+        adjustTextSize();
+    }
+
+    @Override
+    public void setSingleLine(final boolean singleLine) {
+        super.setSingleLine(singleLine);
+        if (singleLine)
+            maxLines = 1;
+        else maxLines = NO_LINE_LIMIT;
+        adjustTextSize();
+    }
+
+    @Override
+    public void setLines(final int lines) {
+        super.setLines(lines);
+        maxLines = lines;
+        adjustTextSize();
+    }
+
+    @Override
+    public void setTextSize(final int unit, final float size) {
+        final Context c = getContext();
+        Resources r;
+        if (c == null)
+            r = Resources.getSystem();
+        else r = c.getResources();
+        maxTextSize = TypedValue.applyDimension(unit, size, r.getDisplayMetrics());
+        adjustTextSize();
+    }
+
+    @Override
+    public void setLineSpacing(final float add, final float mult) {
+        super.setLineSpacing(add, mult);
+        spacingMult = mult;
+        spacingAdd = add;
+    }
+
+    public void setMinTextSize(final float minTextSize) {
+        this.minTextSize = minTextSize;
+        adjustTextSize();
+    }
+
+    private void adjustTextSize() {
+        if (!initialized)
+            return;
+        final int startSize = (int) minTextSize;
+        final int heightLimit = getMeasuredHeight() - getCompoundPaddingBottom() - getCompoundPaddingTop();
+        widthLimit = getMeasuredWidth() - getCompoundPaddingLeft() - getCompoundPaddingRight();
+        if (widthLimit <= 0)
+            return;
+        paint = new TextPaint(getPaint());
+        availableSpaceRect.right = widthLimit;
+        availableSpaceRect.bottom = heightLimit;
+        superSetTextSize(startSize);
+
+    }
+
+    private void superSetTextSize(int startSize) {
+        int textSize = binarySearch(startSize, (int) maxTextSize, sizeTester, availableSpaceRect);
+        super.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+    }
+
+    private int binarySearch(final int start, final int end, final SizeTester sizeTester, final RectF availableSpace) {
+        int lastBest = start, lo = start, hi = end - 1, mid;
+        while (lo <= hi) {
+            mid = lo + hi >>> 1;
+            final int midValCmp = sizeTester.onTestSize(mid, availableSpace);
+            if (midValCmp < 0) {
+                lastBest = lo;
+                lo = mid + 1;
+            } else if (midValCmp > 0) {
+                hi = mid - 1;
+                lastBest = hi;
+            } else return mid;
+        }
+
+        return lastBest;
+    }
+
+    @Override
+    protected void onTextChanged(final CharSequence text, final int start, final int before, final int after) {
+        super.onTextChanged(text, start, before, after);
+        adjustTextSize();
+    }
+
+    @Override
+    protected void onSizeChanged(final int width, final int height, final int oldwidth, final int oldheight) {
+        super.onSizeChanged(width, height, oldwidth, oldheight);
+        if (width != oldwidth || height != oldheight)
+            adjustTextSize();
+    }
+
+}
