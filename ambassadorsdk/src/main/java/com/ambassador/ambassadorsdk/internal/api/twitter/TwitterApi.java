72c4b7f84f6e10cc7de4543bf861e1179761d2bd
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-02 10:09:54 -0500

    [#114778063] setup basic TwitterApi class

---
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/api/twitter/TwitterApi.java
@@ -0,0 +1,36 @@
+package com.ambassador.ambassadorsdk.internal.api.twitter;
+
+import com.ambassador.ambassadorsdk.R;
+import com.ambassador.ambassadorsdk.internal.api.RequestManager;
+import com.ambassador.ambassadorsdk.internal.utils.res.StringResource;
+
+import twitter4j.AsyncTwitter;
+import twitter4j.AsyncTwitterFactory;
+
+/**
+ * Handles Twitter API using Twitter4j.
+ */
+public final class TwitterApi {
+
+    /** The AsyncTwitter Twitter4j object used to make requests. */
+    protected AsyncTwitter twitter;
+
+    /**
+     * Default constructor.
+     * Sets up the AsyncTwitter object to use on all requests.
+     */
+    public TwitterApi() {
+        twitter = new AsyncTwitterFactory().getInstance();
+        String twitterConsumerKey = new StringResource(R.string.twitter_consumer_key).getValue();
+        String twitterConsumerSecret = new StringResource(R.string.twitter_consumer_secret).getValue();
+        twitter.setOAuthConsumer(twitterConsumerKey, twitterConsumerSecret);    }
+
+    /**
+     * Requests a url from Twitter to use for OAuth authentication on a WebView.
+     * @param requestCompletion the request completion callback.
+     */
+    public void getLoginUrl(RequestManager.RequestCompletion requestCompletion) {
+
+    }
+
+}
