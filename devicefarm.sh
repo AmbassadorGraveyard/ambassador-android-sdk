1dd155d210a2784cefc8f420046be1ad1367f61f
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-03 18:01:25 -0500

    [#114127383] @RunUiTests add name argument

--- devicefarm.sh
+++ devicefarm.sh
@@ -6,6 +6,6 @@ msg=`git log -1 --pretty=%B`;
 
 if [[ $msg == *"@RunUiTests"* ]]
 then
-	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN $CIRCLE_TEST_REPORTS/ambassadorsdk-demo-debug.apk --type ANDROID_APP`;
+	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_TEST_REPORTS/ambassadorsdk-demo-debug.apk --type ANDROID_APP`;
 	echo $ret;
 fi
