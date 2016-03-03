1ae05565017ee2db60721e00ef5a2758a7006ff3
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-03 10:26:01 -0500

    [#114778063] finish twitter

--- ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/api/twitter/TwitterApi.java
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/api/twitter/TwitterApi.java
@@ -9,12 +9,16 @@ import twitter4j.AsyncTwitterFactory;
 import twitter4j.TwitterAdapter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterMethod;
+import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
 /**
  * Handles Twitter API using Twitter4j.
  */
 public final class TwitterApi {
+
+    /** Keep track of a requestToken for authentication requests. */
+    protected RequestToken requestToken;
 
     /**
      * Default constructor.
@@ -50,6 +54,7 @@ public final class TwitterApi {
             public void gotOAuthRequestToken(RequestToken token) {
                 super.gotOAuthRequestToken(token);
                 if (complete) return;
+                requestToken = token;
                 requestCompletion.onSuccess(token.getAuthenticationURL());
                 complete = true;
             }
@@ -63,6 +68,33 @@ public final class TwitterApi {
         });
 
         twitter.getOAuthRequestTokenAsync();
+    }
+
+    /**
+     * Trades an oauthVerifier token for an AccessToken using Twitter4j.
+     * @param oauthVerifier the verifier token received from OAuth authenticating.
+     * @param requestCompletion the request completion callback to hand back the AccessToken.
+     */
+    public void getAccessToken(String oauthVerifier, final RequestManager.RequestCompletion requestCompletion) {
+        AsyncTwitter twitter = getTwitter();
+        twitter.addListener(new TwitterAdapter() {
+
+            @Override
+            public void gotOAuthAccessToken(AccessToken token) {
+                super.gotOAuthAccessToken(token);
+                requestCompletion.onSuccess(token);
+            }
+
+            @Override
+            public void onException(TwitterException te, TwitterMethod method) {
+                super.onException(te, method);
+                requestCompletion.onFailure(null);
+            }
+
+        });
+
+        if (requestToken == null) return;
+        twitter.getOAuthAccessTokenAsync(requestToken, oauthVerifier);
     }
 
 }
