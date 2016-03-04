be476e28f273c64a57cb53958e5aa66cec09fce4
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-04 09:29:11 -0500

    [#114127383] @RunUiTests move yml logic into bash

--- devicefarm.sh
+++ devicefarm.sh
@@ -6,6 +6,8 @@ msg=`git log -1 --pretty=%B`;
 
 if [[ $msg == *"@RunUiTests"* ]]
 then
+    ./gradlew -p ambassadorsdk-demo assembleDebug;
+    cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_TEST_REPORTS/app.apk;
 	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_TEST_REPORTS/ambassadorsdk-demo-debug.apk --type ANDROID_APP`;
 	echo $ret;
 fi
