5a7f66415a0da69975c27480327bb0b68acc2c73
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-04 09:56:51 -0500

    [#114127383] @RunUiTests fix string merging

--- devicefarm.sh
+++ devicefarm.sh
@@ -7,7 +7,8 @@ msg=`git log -1 --pretty=%B`;
 if [[ $msg == *"@RunUiTests"* ]]
 then
     ./gradlew -p ambassadorsdk-demo assembleDebug;
-    cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_TEST_REPORTS/app.apk;
+    APP_NAME=`date +%s`; APP_NAME+='.apk';
+    cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_TEST_REPORTS/$APP_NAME;
 	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_TEST_REPORTS/ambassadorsdk-demo-debug.apk --type ANDROID_APP`;
 	uploadArn=`echo $ret | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
 fi
