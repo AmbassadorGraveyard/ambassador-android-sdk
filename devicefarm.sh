6006aa0086d305cf898b653262ea4443fdb3fc46
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-04 10:50:02 -0500

    [#114127383] @RunUiTests refactor

--- devicefarm.sh
+++ devicefarm.sh
@@ -12,33 +12,40 @@ branch=`git rev-parse --abbrev-ref HEAD`;
 # If "RunUiTests" in commit msg
 if [[ $msg == *"@RunUiTests"* ]] || [ $branch == "master" ]
 then
+	# Get current time for naming
+	TIME=`date +%s`;
+
 	# Build the APK
 	./gradlew -p ambassadorsdk-demo assembleDebug;
 
 	# Create new name for APK as current epoch time + '.apk'
-	APP_NAME=`date +%s`; APP_NAME+='.apk';
+	APP_NAME=$TIME; APP_NAME+='.apk';
 
 	# Copy APK to artifacts directory with new name
 	cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_ARTIFACTS/$APP_NAME;
 
 	# Upload APK to AWS Device Farm and store returned JSON
-	apkUpload=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$APP_NAME --type ANDROID_APP`;
+	APK_UPLOAD=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$APP_NAME --type ANDROID_APP`;
 
 	# Extract ARN from the upload JSON
-	apkArn=`echo $apkUpload | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
+	APK_ARN=`echo $APK_UPLOAD | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
 
 	# Create a new name for JAR as current epoch time + '.jar'
-	JAR_NAME=`date +%s`; JAR_NAME+='.jar';
+	JAR_NAME=$TIME; JAR_NAME+='.jar';
 
 	# Copy JAR to artifacts directory with new name
 	cp ./ambassadorsdk-demo/build/intermediates/transforms/jarMerging/debug/jars/1/1f/combined.jar $CIRCLE_ARTIFACTS/$JAR_NAME;
 
 	# Upload JAR to AWS Device Farm and store returned JSON
-	jarUpload=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$JAR_NAME --type UIAUTOMATOR_TEST_PACKAGE`;
+	JAR_UPLOAD=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$JAR_NAME --type UIAUTOMATOR_TEST_PACKAGE`;
 
 	# Extract ARN from the upload JSON
-	jarArn=`echo $jarUpload | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
+	JAR_ARN=`echo $JAR_UPLOAD | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
 
-	echo $apkArn;
-	echo $jarArn;
+	# Create a name for the test run
+	RUN_NAME=$TIME;
+
+	# Start AWS test run
+	#aws devicefarm schedule-run --project-arn $AWS_PROJECT_ARN --app-arn $APK_ARN ---type UIAUTOMATOR
+
 fi
