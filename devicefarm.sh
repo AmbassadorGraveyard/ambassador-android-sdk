6d72b784266a1deee20cd58fc6eb26fd7c2a2c84
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-04 10:02:06 -0500

    [#114127383] fix aws file upload name

--- devicefarm.sh
+++ devicefarm.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
+# Fail on any command failure
 set -o errexit;
 
+# Get the commit msg
 msg=`git log -1 --pretty=%B`;
 
+# If "RunUiTests" in commit msg
 if [[ $msg == *"@RunUiTests"* ]]
 then
+	# Build the APK
     ./gradlew -p ambassadorsdk-demo assembleDebug;
+
+    # Rename APK to current epoch time
     APP_NAME=`date +%s`; APP_NAME+='.apk';
-    cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_TEST_REPORTS/$APP_NAME;
-	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_TEST_REPORTS/ambassadorsdk-demo-debug.apk --type ANDROID_APP`;
+
+    # Copy APK to artifacts directory with new name
+    cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_ARTIFACTS/$APP_NAME;
+
+    # Upload APK to AWS Device Farm and store returned JSON
+	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$APP_NAME --type ANDROID_APP`;
+
+	# Extract ARN from the upload JSON
 	uploadArn=`echo $ret | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
 fi
