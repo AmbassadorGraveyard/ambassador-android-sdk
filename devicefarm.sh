acfd09af64233069488a08611d67fdfda5a6e06a
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-04 09:39:15 -0500

    [#114127383] test get arn

--- devicefarm.sh
+++ devicefarm.sh
@@ -9,5 +9,5 @@ then
     ./gradlew -p ambassadorsdk-demo assembleDebug;
     cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_TEST_REPORTS/app.apk;
 	ret=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_TEST_REPORTS/ambassadorsdk-demo-debug.apk --type ANDROID_APP`;
-	echo $ret;
+	echo $ret | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["upload"]["arn"]'
 fi
