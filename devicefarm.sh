a204485862821ed7acd49ac0870808a569c79fd2
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-08 15:23:19 -0500

    [#114127383] do uploads

--- devicefarm.sh
+++ devicefarm.sh
@@ -4,10 +4,11 @@
 abort()
 {
 	# Set a commit fail status for device farm if any error causes the script to abort.
-	echo '{"state":"failure","target_url":"http://google.com","description":"An error occurred with device farm execution.","context":"aws/devicefarm"}' | curl -d @- https://api.github.com/repos/GetAmbassador/ambassador-android-sdk/statuses/$sha?access_token=$GITHUB_ACCESS_TOKEN;
+	report_github_status "failure" "A fatal error occurred with device farm test execution.";
 	exit 0;
 }
 
+# Call abort on exit
 trap 'abort' 0;
 
 # Fail on any command error
@@ -22,6 +23,12 @@ branch=`git rev-parse --abbrev-ref HEAD`;
 # Get commit sha for GitHub reporting
 sha=`git rev-parse HEAD`;
 
+# Function to report a github commit status
+report_github_status() 
+{
+	echo "{\"state\":\"$1\",\"target_url\":\"$CIRCLE_BUILD_URL\",\"description\":\"$2\",\"context\":\"aws/devicefarm\"}" | curl -d @- https://api.github.com/repos/GetAmbassador/ambassador-android-sdk/statuses/$sha?access_token=$GITHUB_ACCESS_TOKEN;
+}
+
 # If "RunUiTests" in commit msg
 if [[ $msg == *"@RunUiTests"* ]] || [ $branch == "master" ]
 then
@@ -32,31 +39,87 @@ then
 	./gradlew -p ambassadorsdk-demo assembleDebug;
 
 	# Create new name for app APK as current epoch time + '.apk'
-	APP_NAME=$TIME; APP_NAME+='.apk';
+	APK_NAME="$TIME.apk";
 
 	# Copy APK to artifacts directory with new name
-	cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_ARTIFACTS/$APP_NAME;
+	cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_ARTIFACTS/$APK_NAME;
 
-	# Upload APK to AWS Device Farm and store returned JSON
-	APK_UPLOAD=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$APP_NAME --type ANDROID_APP`;
+	# Request APK upload to AWS Device Farm and store returned JSON
+	APK_UPLOAD=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$APK_NAME --type ANDROID_APP`;
 
-	# Extract ARN from the upload JSON
+	# Extract ARN from the response JSON
 	APK_ARN=`echo $APK_UPLOAD | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
+
+	# Get the remote url to upload the app APK to
+	APK_UPLOAD_URL=`echo $APK_UPLOAD | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["url"]'`;
+
+	# Upload APK to AWS Device Farm
+	curl -T $CIRCLE_ARTIFACTS/$APK_NAME $APK_UPLOAD_URL;
+
+	# Wait for upload success
+	while true; do
+		# Get upload info from AWS using ARN
+		GET_UPLOAD=`aws devicefarm get-upload --arn $APK_ARN`;
+
+		# Get status from respone JSON
+		UPLOAD_STATUS=`echo $GET_UPLOAD| python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["status"]'`;
+
+		# Break loop if success
+		if [ $UPLOAD_STATUS == "SUCCEEDED" ]
+		then
+			abort;
+			break;
+		elif [ $UPLOAD_STATUS == "FAILED" ]
+		then
+			break;
+		fi
+
+		# Wait 3 seconds before checking again
+		sleep 3;
+	done
 
 	# Build the tests APK
 	./gradlew -p ambassadorsdk-demo assembleAndroidTest;
 
 	# Create a new name for tests APK as current epoch time + '.apk'
-	TESTS_NAME=$TIME; TESTS_NAME+='_tests.apk';
+	TESTS_NAME="$TIME-tests.apk";
 
 	# Copy APK to artifacts directory with new name
 	cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug-androidTest-unaligned.apk $CIRCLE_ARTIFACTS/$TESTS_NAME;
 
-	# Upload APK to AWS Device Farm and store returned JSON
+	# Request APK upload to AWS Device Farm and store response JSON
 	TESTS_UPLOAD=`aws devicefarm create-upload --project-arn $AWS_PROJECT_ARN --name $CIRCLE_ARTIFACTS/$TESTS_NAME --type INSTRUMENTATION_TEST_PACKAGE`;
 
-	# Extract ARN from the upload JSON
+	# Extract ARN from the response JSON
 	TESTS_ARN=`echo $TESTS_UPLOAD | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["arn"]'`;
+
+	#Get the remote url to upload the tests APK to
+	TESTS_UPLOAD_URL=`echo $APK_UPLOAD | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["url"]'`;
+
+	# Upload APK to AWS Device Farm
+	curl -T $CIRCLE_ARTIFACTS/$TESTS_NAME $TESTS_UPLOAD_URL;
+
+	# Wait for upload success
+	while true; do
+		# Get upload info from AWS using ARN
+		GET_UPLOAD=`aws devicefarm get-upload --arn $TESTS_ARN`;
+
+		# Get status from respone JSON
+		UPLOAD_STATUS=`echo $GET_UPLOAD| python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload"]["status"]'`;
+
+		# Break loop if success
+		if [ $UPLOAD_STATUS == "SUCCEEDED" ]
+		then
+			abort;
+			break;
+		elif [ $UPLOAD_STATUS == "FAILED" ]
+		then
+			break;
+		fi
+
+		# Wait 3 seconds before checking again
+		sleep 3;
+	done
 
 	# Create a name for the test run
 	RUN_NAME="test$TIME";
@@ -68,10 +131,7 @@ then
 	TEST_RESULT=`aws devicefarm schedule-run --project-arn "$AWS_PROJECT_ARN" --app-arn "$APK_ARN" --device-pool-arn "$AWS_DEVICE_POOL_ARN" --name "$RUN_NAME" --test "$TEST_INFO"`
 
 	# Send GitHub status
-	STATUS="success";
-	TARGET_URL="http://google.com";
-	DESCRIPTION="This test build succeeded";
-	echo "{\"state\":\"$STATUS\",\"target_url\":\"$TARGET_URL\",\"description\":\"$DESCRIPTION\",\"context\":\"aws/devicefarm\"}" | curl -d @- https://api.github.com/repos/GetAmbassador/ambassador-android-sdk/statuses/$sha?access_token=$GITHUB_ACCESS_TOKEN;
+	report_github_status "success" "This test build succeeded";
 
 	echo $TEST_RESULT
 else
@@ -79,7 +139,8 @@ else
 	echo "Tests not running. To run tests outside of master add @RunUiTests to the commit message.";
 
 	# Set failure commit status whenever tests not run. This way a merge can only ever happen if UI tests are run and pass.
-	echo '{"state":"failure","target_url":"http://google.com","description":"Instrumentation tests not run.","context":"aws/devicefarm"}' | curl -d @- https://api.github.com/repos/GetAmbassador/ambassador-android-sdk/statuses/$sha?access_token=$GITHUB_ACCESS_TOKEN;
+	report_github_status "failure" "Instrumentation tests not run.";
 fi
 
+# End abort on exit block
 trap : 0
