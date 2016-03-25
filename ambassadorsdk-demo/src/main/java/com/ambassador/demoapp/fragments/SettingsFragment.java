bd374df02e32c34c15e0d452562405a08a56108f
Dylan McIntyre <dylan@dmcintyre.net>
2016-03-25 11:38:36 -0400

    [#116251427] conversion basic design

--- ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/fragments/SettingsFragment.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/fragments/SettingsFragment.java
@@ -1,29 +1,17 @@
 package com.ambassador.demoapp.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.ambassador.demoapp.Demo;
 import com.ambassador.demoapp.R;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public final class SettingsFragment extends Fragment {
-
-    @Bind(R.id.etEmail)     protected EditText  etEmail;
-    @Bind(R.id.etUsername)  protected EditText  etUsername;
-    @Bind(R.id.etPassword)  protected EditText  etPassword;
-    @Bind(R.id.btnSignup)   protected Button    btnSignup;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,36 +25,7 @@ public final class SettingsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         ButterKnife.bind(this, view);
 
-        btnSignup.setOnClickListener(btnLoginOnClickListener);
-
         return view;
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        etEmail.requestFocus();
-        closeSoftKeyboard();
-    }
-
-    private void closeSoftKeyboard() {
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(getActivity().findViewById(android.R.id.content).getWindowToken(), 0);
-    }
-
-    protected View.OnClickListener btnLoginOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (etEmail.getText().length() != 0 && etUsername.getText().length() != 0 && etPassword.getText().length() != 0) {
-                String email = etEmail.getText().toString();
-                String username = etUsername.getText().toString();
-                Toast.makeText(getActivity(), "Signing up!", Toast.LENGTH_LONG).show();
-                Demo.get().signupConversion(email, username);
-                closeSoftKeyboard();
-            } else {
-                Toast.makeText(getActivity(), "Please enter an email, username, and password!", Toast.LENGTH_LONG).show();
-            }
-        }
-    };
 
 }
