fe342128a3576e803a6de6c605d549068964b410
Dylan McIntyre <dylan@dmcintyre.net>
2016-04-04 13:14:24 -0400

    [#115074555] create integration on save

--- ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/data/Integration.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/data/Integration.java
@@ -5,7 +5,44 @@ import com.ambassador.ambassadorsdk.RAFOptions;
 public class Integration {
 
     protected String name;
+    protected int campaignId;
     protected RAFOptions rafOptions;
     protected long createdAtDate;
+
+    public Integration() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(int campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    public RAFOptions getRafOptions() {
+        return rafOptions;
+    }
+
+    public void setRafOptions(RAFOptions rafOptions) {
+        this.rafOptions = rafOptions;
+    }
+
+    public long getCreatedAtDate() {
+        return createdAtDate;
+    }
+
+    public void setCreatedAtDate(long createdAtDate) {
+        this.createdAtDate = createdAtDate;
+    }
 
 }
