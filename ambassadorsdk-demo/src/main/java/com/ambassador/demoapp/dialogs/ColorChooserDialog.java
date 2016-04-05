009dd2367a8a114195dbe55e6b3927a7c8abff6a
Dylan McIntyre <dylan@dmcintyre.net>
2016-04-05 09:55:59 -0400

    [#115074555] refactor color chooser

--- ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/dialogs/ColorChooserDialog.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/dialogs/ColorChooserDialog.java
@@ -10,8 +10,6 @@ import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,32 +21,58 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ambassador.demoapp.R;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+/**
+ * Dialog that allows the user to select a color. For proper experience you need to use setColor(...)
+ * once before showing the dialog. Selected color can be retrieved on dismiss with getColor().
+ */
 public class ColorChooserDialog extends Dialog {
 
+    /** Boolean telling if views inflated and binded. */
     protected boolean inflated = false;
 
+    /** The color int that the dialog launched with. */
+    @ColorInt protected int launchColor = -1;
+
+    /** The currently selected color. */
     @ColorInt protected int color;
-    @ColorInt protected int hueExtreme;
+
+    /** Red component value of color. Between 0 and 255. */
     protected int r;
+
+    /** Green component value of color. Between 0 and 255. */
     protected int g;
+
+    /** Blue component value of color. Between 0 and 255. */
     protected int b;
+
+    /** Hue component value of color. Between 0 and 360. */
     protected float h;
+
+    /** Saturation component value of color. Between 0 and 1. */
     protected float s;
+
+    /** Value component value of color. Between 0 and 1. */
     protected float v;
 
+    /** The color to draw the gradient with. The hue extreme that allows you to select color. */
+    @ColorInt protected int hueExtreme;
+
+    /** X position of the current selected color on the chooser. */
     protected int colorX = 50;
+
+    /** Y position of the current selected color on the chooser. */
     protected int colorY = 50;
+
+    protected GradientDrawable currentColorMarker;
 
     @Bind(R.id.viewColorSpot) protected View colorSpot;
     @Bind(R.id.rlColors) protected RelativeLayout rlColors;
@@ -79,40 +103,36 @@ public class ColorChooserDialog extends Dialog {
         ButterKnife.bind(this);
         inflated = true;
         updateGradients(hueExtreme);
-        setUpRainbow();
+
+        // Setup the rainbow gradient for selecting hue.
+        int[] colors = new int[]{ Color.RED, Color.GREEN, Color.BLUE, Color.RED };
+        for (int i = 0; i < colors.length - 1; i++) {
+            View view = new View(getOwnerActivity());
+            view.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+            GradientDrawable background = new GradientDrawable();
+            background.setOrientation(GradientDrawable.Orientation.LEFT_RIGHT);
+            background.setColors(new int[]{ colors[i], colors[i+1] });
+            view.setBackground(background);
+            llRainbow.addView(view);
+        }
 
         GradientDrawable colorSpotBackground = new GradientDrawable();
         colorSpotBackground.setStroke(2, Color.BLACK);
         hueTracker.setBackground(colorSpotBackground);
 
-        flColorA.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        // Wait until color gradients inflated and set the current colorX and colorY and update views.
+        rlColors.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                int colorSpotX = (int) (flColorA.getWidth() * s);
-                int colorSpotY = flColorA.getHeight() - (int) (flColorA.getHeight() * v);
-                colorSpot.setTranslationX(colorSpotX - colorSpot.getWidth() / 2);
-                colorSpot.setTranslationY(colorSpotY - colorSpot.getHeight() / 2);
-
-                if (colorSpotY < flColorA.getHeight() / 2) {
-                    GradientDrawable colorSpotBackground = new GradientDrawable();
-                    colorSpotBackground.setStroke(2, Color.BLACK);
-                    colorSpotBackground.setCornerRadius(100);
-                    colorSpot.setBackground(colorSpotBackground);
-                } else {
-                    GradientDrawable colorSpotBackground = new GradientDrawable();
-                    colorSpotBackground.setStroke(2, Color.WHITE);
-                    colorSpotBackground.setCornerRadius(100);
-                    colorSpot.setBackground(colorSpotBackground);
-                }
-
-                colorX = colorSpotX;
-                colorY = colorSpotY;
+                colorX = (int) (flColorA.getWidth() * s);
+                colorY = flColorA.getHeight() - (int) (flColorA.getHeight() * v);
+                updateCurrentColorMarker();
                 updateColor();
-
-                flColorA.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                rlColors.getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
         });
 
+        // Get new color when click on colorGradient.
         rlColors.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -123,26 +143,13 @@ public class ColorChooserDialog extends Dialog {
                 colorX = (int) event.getX();
                 colorY = (int) event.getY();
 
-                if (event.getY() < flColorA.getHeight() / 2) {
-                    GradientDrawable colorSpotBackground = new GradientDrawable();
-                    colorSpotBackground.setStroke(2, Color.BLACK);
-                    colorSpotBackground.setCornerRadius(100);
-                    colorSpot.setBackground(colorSpotBackground);
-                } else {
-                    GradientDrawable colorSpotBackground = new GradientDrawable();
-                    colorSpotBackground.setStroke(2, Color.WHITE);
-                    colorSpotBackground.setCornerRadius(100);
-                    colorSpot.setBackground(colorSpotBackground);
-                }
-
-                colorSpot.setTranslationX(event.getX() - colorSpot.getWidth() / 2);
-                colorSpot.setTranslationY(event.getY() - colorSpot.getHeight() / 2);
-
+                updateCurrentColorMarker();
                 updateColor();
                 return true;
             }
         });
 
+        // Get new hue and update everything when click on hue slider.
         llRainbow.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -154,52 +161,53 @@ public class ColorChooserDialog extends Dialog {
                 llRainbow.buildDrawingCache();
                 final Bitmap colors = llRainbow.getDrawingCache();
                 int pixel = colors.getPixel((int) event.getX(), (int) event.getY());
-                updateGradients(pixel);
-
                 llRainbow.setDrawingCacheEnabled(false);
                 hueTracker.setTranslationX(event.getX());
 
+                updateGradients(pixel);
                 updateColor();
 
                 return true;
             }
         });
 
+        // Reset to launch color and dismiss when cancel clicked.
         tvColorCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setColor(launchColor);
                 dismiss();
             }
         });
 
+        // Dismiss when done clicked.
         tvColorDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvColorDone.setBackgroundColor(Color.parseColor("#eeeeee"));
                 dismiss();
             }
         });
 
+        // Set TextWatchers on input to update other stuff when edited.
         etRedValue.addTextChangedListener(rgbTextWatcher);
         etGreenValue.addTextChangedListener(rgbTextWatcher);
         etBlueValue.addTextChangedListener(rgbTextWatcher);
-
-        //etHexValue.addTextChangedListener(hexTextWatcher);
+        etHexValue.addTextChangedListener(hexTextWatcher);
     }
 
-    protected void setUpRainbow() {
-        int[] colors = new int[]{ Color.RED, Color.GREEN, Color.BLUE, Color.RED };
-        for (int i = 0; i < colors.length - 1; i++) {
-            View view = new View(getOwnerActivity());
-            view.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
-            GradientDrawable background = new GradientDrawable();
-            background.setOrientation(GradientDrawable.Orientation.LEFT_RIGHT);
-            background.setColors(new int[]{ colors[i], colors[i+1] });
-            view.setBackground(background);
-            llRainbow.addView(view);
-        }
+    /**
+     * Returns the selected ColorInt.
+     */
+    @ColorInt
+    public int getColor() {
+        return color;
     }
 
+    /**
+     * Updates the color selection gradient with a certain color. Draws a gradient with this color to
+     * white from right to left. Draws a gradient with black to transparent from the bottom up.
+     * @param color the ColorInt to base the gradients on. Should have S and V values of 1.
+     */
     protected void updateGradients(@ColorInt int color) {
         GradientDrawable a = new GradientDrawable();
         a.setColors(new int[]{ color, Color.WHITE });
@@ -212,32 +220,19 @@ public class ColorChooserDialog extends Dialog {
         flColorB.setBackground(b);
     }
 
-    protected void updateColor() {
-        rlColors.setDrawingCacheEnabled(true);
-        rlColors.buildDrawingCache();
-        final Bitmap colors = rlColors.getDrawingCache();
-        color = colors.getPixel(colorX, colorY);
-        rlColors.setDrawingCacheEnabled(false);
-
-        int red = Color.red(color);
-        int green = Color.green(color);
-        int blue = Color.blue(color);
-
-        etRedValue.setText(String.valueOf(red));
-        etGreenValue.setText(String.valueOf(green));
-        etBlueValue.setText(String.valueOf(blue));
-        etHexValue.setText(String.format("%06X", (0xFFFFFF & color)));
-
-        viewPreview.setBackgroundColor(color);
-    }
-
-    @ColorInt
-    public int getColor() {
-        return color;
-    }
-
+    /**
+     * Sets the current color on the dialog. This sets all member fields according to that color.
+     * Sets the color field. Sets the launchColor field if not set yet. Saves R,G,B values and H,S,V
+     * values. Saves the hue extreme ColorInt.
+     * Does not modify any views.
+     * @param color the ColorInt integer to set.
+     */
     public void setColor(@ColorInt int color) {
         this.color = color;
+
+        if (this.launchColor == -1) {
+            this.launchColor = color;
+        }
 
         r = Color.red(color);
         g = Color.green(color);
@@ -251,6 +246,45 @@ public class ColorChooserDialog extends Dialog {
         v = hsv[2];
 
         hueExtreme = Color.HSVToColor(new float[]{ h, 1, 1 });
+    }
+
+    /**
+     * Uses colorX and colorY to determine the current selected color. Sets the RGB and hex values
+     * on the inputs and updates the preview rectangle.
+     */
+    protected void updateColor() {
+        rlColors.setDrawingCacheEnabled(true);
+        rlColors.buildDrawingCache();
+        final Bitmap colors = rlColors.getDrawingCache();
+        setColor(colors.getPixel(colorX, colorY));
+        rlColors.setDrawingCacheEnabled(false);
+
+        etRedValue.setText(String.valueOf(r));
+        etGreenValue.setText(String.valueOf(g));
+        etBlueValue.setText(String.valueOf(b));
+        etHexValue.setText(String.format("%06X", (0xFFFFFF & color)));
+
+        viewPreview.setBackgroundColor(color);
+    }
+
+    protected GradientDrawable getCurrentColorMarker(@ColorInt int color) {
+        if (currentColorMarker == null) {
+            currentColorMarker = new GradientDrawable();
+            currentColorMarker.setCornerRadius(100);
+        }
+
+        currentColorMarker.setStroke(2, color);
+        return currentColorMarker;
+    }
+
+    protected void updateCurrentColorMarker() {
+        colorSpot.setTranslationX(colorX - colorSpot.getWidth() / 2);
+        colorSpot.setTranslationY(colorY - colorSpot.getHeight() / 2);
+        if (colorY < flColorA.getHeight() / 2) {
+            colorSpot.setBackground(getCurrentColorMarker(Color.BLACK));
+        } else {
+            colorSpot.setBackground(getCurrentColorMarker(Color.WHITE));
+        }
     }
 
     protected TextWatcher rgbTextWatcher = new TextWatcher() {
@@ -281,24 +315,7 @@ public class ColorChooserDialog extends Dialog {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            List<String> allowedCharacters = Arrays.asList("A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-            String hex = etHexValue.getText().toString();
-            if (hex.length() == 6) {
-                boolean validHex = true;
-                for (int i = 0; i < hex.length(); i++) {
-                    String character = String.valueOf(hex.charAt(i));
-                    if (!allowedCharacters.contains(character)) {
-                        validHex = false;
-                        break;
-                    }
-                }
 
-                if (validHex) {
-                    int color = Color.parseColor("#" + hex);
-                    setColor(color);
-                    updateColor();
-                }
-            }
         }
 
         @Override
