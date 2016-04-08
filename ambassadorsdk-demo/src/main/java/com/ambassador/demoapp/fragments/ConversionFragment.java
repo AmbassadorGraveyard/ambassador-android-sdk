f96847561e74c65bf85497ae318bf7923f411b9f
Dylan McIntyre <dylan@dmcintyre.net>
2016-04-08 10:16:39 -0400

    [#116316945] Small export fixes (quotes/newlines)

--- ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/fragments/ConversionFragment.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/demoapp/fragments/ConversionFragment.java
@@ -131,10 +131,10 @@ public final class ConversionFragment extends Fragment implements MainActivity.T
         identifyBuilder.append("    @Override\n");
         identifyBuilder.append("    public void onCreate() {\n");
         identifyBuilder.append("        super.onCreate();\n");
-        identifyBuilder.append(String.format("        AmbassadorSDK.runWithKeys(this, %s, %s);\n", User.get().getSdkToken(), User.get().getUniversalId()));
+        identifyBuilder.append(String.format("        AmbassadorSDK.runWithKeys(this, \"%s\", \"%s\");\n", User.get().getSdkToken(), User.get().getUniversalId()));
         identifyBuilder.append("        ConversionParameters conversionParameters = new ConversionParameters.Builder()\n");
         identifyBuilder.append(getConversionParametersAdditionLines());
-        identifyBuilder.append("            .build();");
+        identifyBuilder.append("            .build();\n");
         identifyBuilder.append("        AmbassadorSDK.registerConversion(conversionParameters);\n");
         identifyBuilder.append("    }\n\n");
         identifyBuilder.append("}");
