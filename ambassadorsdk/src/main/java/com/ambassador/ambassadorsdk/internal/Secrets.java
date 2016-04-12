d27e534eb51d21526c0443ce757c302593b25b7d
Dylan McIntyre <dylan@dmcintyre.net>
2016-04-12 09:55:42 -0400

    [#115194067] replace secret usages

--- ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/Secrets.java
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/Secrets.java
@@ -4,11 +4,11 @@ import com.ambassador.ambassadorsdk.BuildConfig;
 
 public final class Secrets {
 
-    public String getAugurKey() {
+    public static String getAugurKey() {
         return BuildConfig.AUGUR_KEY;
     }
 
-    public String getPusherKey() {
+    public static String getPusherKey() {
         return BuildConfig.IS_RELEASE_BUILD ? BuildConfig.PUSHER_KEY_PROD : BuildConfig.PUSHER_KEY_DEV;
     }
 
