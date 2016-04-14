bfd02e00cd54851f079902291be68c04e0b5d665
Dylan McIntyre <dylan@dmcintyre.net>
2016-04-14 12:09:28 -0400

    [#117480261] Conversion readme

--- ambassadorsdk-demo/src/main/java/com/ambassador/demo/exports/ConversionExport.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/demo/exports/ConversionExport.java
@@ -6,7 +6,20 @@ public class ConversionExport extends BaseExport<ConversionParameters> {
 
     @Override
     public String getReadme() {
-        return null;
+        PlaintextFile readme = new PlaintextFile();
+        readme.addLine("Ambassador Android SDK v1.1.4");
+        readme.addLine("Take a look at the Android docs for an in-depth explanation on adding and integrating the SDK:");
+        readme.addLine("https://docs.getambassador.com/v2.0.0/page/android-sdk");
+        readme.addLine("Check out the MyApplication.java file for an example of this conversion request.");
+
+        readme.addLine("");
+
+        readme.addLine("Ambassador iOS SDK v1.0.3");
+        readme.addLine("Take a look at the iOS docs for an in-depth explanation on adding and integrating the SDK:");
+        readme.addLine("https://docs.getambassador.com/v2.0.0/page/ios-sdk");
+        readme.addLine("Check out the AppDelegate.m or AppDelegate.swift files for examples of this conversion request.");
+
+        return readme.get();
     }
 
     @Override
