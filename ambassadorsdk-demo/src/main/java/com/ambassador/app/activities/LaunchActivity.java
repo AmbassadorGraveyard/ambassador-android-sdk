664fe11b6d165707b24400a9dfa1d61af71ca4ba
Dylan McIntyre <dylan@dmcintyre.net>
2016-05-03 10:32:56 -0400

    [#118387287] tests

--- ambassadorsdk-demo/src/main/java/com/ambassador/app/activities/LaunchActivity.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/app/activities/LaunchActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.ambassador.ambassadorsdk.internal.activities.survey.SurveyActivity;
 import com.ambassador.app.data.User;
 
 public class LaunchActivity extends Activity {
@@ -13,7 +12,6 @@ public class LaunchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Class<?> activity = User.isStored() ? MainActivity.class : LoginActivity.class;
-        activity = SurveyActivity.class;
         Intent intent = new Intent(this, activity)
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
---
+++ ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/internal/activities/BasePresenterTest.java
@@ -0,0 +1,60 @@
+package com.ambassador.ambassadorsdk.internal.activities;
+
+import android.view.View;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.lang.ref.WeakReference;
+
+@RunWith(PowerMockRunner.class)
+public class BasePresenterTest {
+
+    protected BasePresenter<Object, Object> basePresenter;
+
+    @Before
+    public void setUp() {
+        basePresenter = new BasePresenter<Object, Object>() {
+            @Override
+            protected void updateView() {
+
+            }
+        };
+
+        basePresenter = Mockito.spy(basePresenter);
+    }
+
+    @Test
+    public void testsSetModelDoesResetAndUpdateWhenSetupDone() {
+        basePresenter.view = new WeakReference<Object>(Mockito.mock(View.class));
+        basePresenter.setModel(new Object());
+        Mockito.verify(basePresenter).resetState();
+        Mockito.verify(basePresenter).updateView();
+    }
+
+    @Test
+    public void testsSetModelDoesResetAndNotUpdateWhenSetupNotDone() {
+        basePresenter.view = new WeakReference<Object>(null);
+        basePresenter.setModel(new Object());
+        Mockito.verify(basePresenter).resetState();
+        Mockito.verify(basePresenter, Mockito.never()).updateView();
+    }
+
+    @Test
+    public void testsBindViewDoesUpdateViewWhenSetupDone() {
+        basePresenter.model = new Object();
+        basePresenter.bindView(Mockito.mock(View.class));
+        Mockito.verify(basePresenter).updateView();
+    }
+
+    @Test
+    public void testsBindViewDoesNotUpdateViewWhenSetupNotDone() {
+        basePresenter.model = null;
+        basePresenter.bindView(Mockito.mock(View.class));
+        Mockito.verify(basePresenter, Mockito.never()).updateView();
+    }
+
+}
