48bb35195be5e9cfc00057ef550e85c2df9f0b8a
Dylan McIntyre <dylan@dmcintyre.net>
2016-05-10 16:31:13 -0400

    [#119047363] new identify method, deprecate old

---
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/identify/AmbassadorIdentification.java
@@ -0,0 +1,136 @@
+package com.ambassador.ambassadorsdk.internal.identify;
+
+public class AmbassadorIdentification {
+
+    protected String email;
+    protected String firstName;
+    protected String lastName;
+    protected String company;
+    protected String phone;
+    protected String street;
+    protected String city;
+    protected String state;
+    protected String postalCode;
+    protected String country;
+    protected String customLabel1;
+    protected String customLabel2;
+    protected String customLabel3;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public AmbassadorIdentification setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public AmbassadorIdentification setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public AmbassadorIdentification setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public AmbassadorIdentification setCompany(String company) {
+        this.company = company;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public AmbassadorIdentification setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public AmbassadorIdentification setStreet(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public AmbassadorIdentification setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public AmbassadorIdentification setState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public AmbassadorIdentification setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+        return this;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public AmbassadorIdentification setCountry(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public String getCustomLabel1() {
+        return customLabel1;
+    }
+
+    public AmbassadorIdentification setCustomLabel1(String customLabel1) {
+        this.customLabel1 = customLabel1;
+        return this;
+    }
+
+    public String getCustomLabel2() {
+        return customLabel2;
+    }
+
+    public AmbassadorIdentification setCustomLabel2(String customLabel2) {
+        this.customLabel2 = customLabel2;
+        return this;
+    }
+
+    public String getCustomLabel3() {
+        return customLabel3;
+    }
+
+    public AmbassadorIdentification setCustomLabel3(String customLabel3) {
+        this.customLabel3 = customLabel3;
+        return this;
+    }
+
+}
