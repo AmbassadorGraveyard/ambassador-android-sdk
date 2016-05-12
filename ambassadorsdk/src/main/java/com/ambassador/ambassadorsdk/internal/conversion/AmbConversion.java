b528eab8e402bcc05e8284e7e913c9f4ed4dd4c3
Dylan McIntyre <dylan@dmcintyre.net>
2016-05-12 08:45:06 -0400

    [#118671807] code to attempt pending conversions

--- ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/conversion/AmbConversion.java
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/conversion/AmbConversion.java
@@ -10,6 +10,7 @@ import com.ambassador.ambassadorsdk.internal.data.Campaign;
 import com.ambassador.ambassadorsdk.internal.data.User;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import javax.inject.Inject;
@@ -32,15 +33,17 @@ public class AmbConversion {
     public void execute() {
         if (conversionParameters.getCampaign() == -1 || conversionParameters.getRevenue() < 0) {
             Log.e("Ambassador", "Campaign and Revenue MUST be set on ConversionParameters!");
-            conversionStatusListener.error();
+            if (conversionStatusListener != null) conversionStatusListener.error();
             return;
         }
 
         if (campaign.getReferredByShortCode() == null || "".equals(campaign.getReferredByShortCode()) || user.getEmail() == null) {
             save();
-            conversionStatusListener.pending();
+            if (conversionStatusListener != null) conversionStatusListener.pending();
+            conversionStatusListener = null;
             return;
         }
+
 
     }
 
@@ -54,6 +57,17 @@ public class AmbConversion {
 
     public static AmbConversion get(ConversionParameters conversionParameters, boolean limitOnce, ConversionStatusListener conversionStatusListener) {
         return new AmbConversion(conversionParameters, limitOnce, conversionStatusListener);
+    }
+
+    public static void attemptExecutePending() {
+        SharedPreferences sharedPreferences = AmbSingleton.getContext().getSharedPreferences("conversions", Context.MODE_PRIVATE);
+        String content = sharedPreferences.getString("conversions", "[]");
+        sharedPreferences.edit().putString("conversions", "[]").apply();
+        final JsonArray conversions = new JsonParser().parse(content).getAsJsonArray();
+        for (final JsonElement jsonElement : conversions) {
+            AmbConversion ambConversion = new Gson().fromJson(jsonElement, AmbConversion.class);
+            ambConversion.execute();
+        }
     }
 
 }
