5d8b70509ca24e9e87dc9ab2c911f0e7c6165466
Dylan McIntyre <dylan@dmcintyre.net>
2016-05-12 07:32:22 -0400

    [#118671807] save AmbConversions to SharedPrefs

--- ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/conversion/AmbConversion.java
+++ ambassadorsdk/src/main/java/com/ambassador/ambassadorsdk/internal/conversion/AmbConversion.java
@@ -1,6 +1,14 @@
 package com.ambassador.ambassadorsdk.internal.conversion;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
 import com.ambassador.ambassadorsdk.ConversionParameters;
+import com.ambassador.ambassadorsdk.internal.AmbSingleton;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 
 public class AmbConversion {
 
@@ -16,9 +24,19 @@ public class AmbConversion {
 
     public void execute() {
         if (conversionParameters.getCampaign() == -1 || conversionParameters.getRevenue() < 0) {
+            Log.e("Ambassador", "Campaign and Revenue MUST be set on ConversionParameters!");
             conversionStatusListener.error();
             return;
         }
+
+    }
+
+    protected void save() {
+        SharedPreferences sharedPreferences = AmbSingleton.getContext().getSharedPreferences("conversions", Context.MODE_PRIVATE);
+        String content = sharedPreferences.getString("conversions", "[]");
+        JsonArray conversions = new JsonParser().parse(content).getAsJsonArray();
+        conversions.add(new JsonParser().parse(new Gson().toJson(conversionParameters)).getAsJsonObject());
+        sharedPreferences.edit().putString("conversions", conversions.toString()).apply();
     }
 
     public static AmbConversion get(ConversionParameters conversionParameters, boolean limitOnce, ConversionStatusListener conversionStatusListener) {
