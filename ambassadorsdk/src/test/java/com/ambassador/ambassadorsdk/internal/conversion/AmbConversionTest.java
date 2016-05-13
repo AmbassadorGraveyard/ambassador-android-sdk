d70c07ce8f786fe64a39def77f8167a8b6400d90
Dylan McIntyre <dylan@dmcintyre.net>
2016-05-13 16:14:00 -0400

    [#118671807] execute pre-check tests

--- ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/internal/conversion/AmbConversionTest.java
+++ ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/internal/conversion/AmbConversionTest.java
@@ -2,9 +2,13 @@ package com.ambassador.ambassadorsdk.internal.conversion;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.ambassador.ambassadorsdk.ConversionParameters;
 import com.ambassador.ambassadorsdk.internal.AmbSingleton;
+import com.ambassador.ambassadorsdk.internal.data.Campaign;
+import com.ambassador.ambassadorsdk.internal.data.User;
+import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 
 import junit.framework.Assert;
@@ -21,20 +25,28 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
-        AmbSingleton.class
+        AmbSingleton.class,
+        Gson.class,
+        Log.class,
 })
 public class AmbConversionTest {
 
     protected Context context;
+    protected Campaign campaign;
+    protected User user;
 
     @Before
     public void setup() {
         PowerMockito.mockStatic(
-                AmbSingleton.class
+                AmbSingleton.class,
+                Log.class
         );
 
         context = Mockito.mock(Context.class);
         PowerMockito.when(AmbSingleton.getContext()).thenReturn(context);
+
+        campaign = Mockito.mock(Campaign.class);
+        user = Mockito.mock(User.class);
     }
 
     @Test
@@ -75,6 +87,100 @@ public class AmbConversionTest {
 
         AmbConversion ambConversion = AmbConversion.get(new ConversionParameters(), false, Mockito.mock(ConversionStatusListener.class));
         ambConversion.save();
+    }
+
+    @Test
+    public void testsExecuteDoesFailOnInvalidCampaign() {
+        ConversionParameters conversionParameters = new ConversionParameters.Builder()
+                .setRevenue(15)
+                .build();
+        ConversionStatusListener conversionStatusListener = Mockito.mock(ConversionStatusListener.class);
+
+        AmbConversion.get(conversionParameters, false, conversionStatusListener).execute();
+
+        Mockito.verify(conversionStatusListener).error();
+    }
+
+    @Test
+    public void testsExecuteDoesFailOnInvalidRevenue() {
+        ConversionParameters conversionParameters = new ConversionParameters.Builder()
+                .setCampaign(260)
+                .setRevenue(-1)
+                .build();
+        ConversionStatusListener conversionStatusListener = Mockito.mock(ConversionStatusListener.class);
+
+        AmbConversion.get(conversionParameters, false, conversionStatusListener).execute();
+
+        Mockito.verify(conversionStatusListener).error();
+    }
+
+    @Test
+    public void testsExecuteDoesPendingOnNullShortCode() {
+        ConversionParameters conversionParameters = new ConversionParameters.Builder()
+                .setCampaign(260)
+                .setRevenue(12)
+                .build();
+        ConversionStatusListener conversionStatusListener = Mockito.mock(ConversionStatusListener.class);
+
+        AmbConversion ambConversion = Mockito.spy(AmbConversion.get(conversionParameters, false, conversionStatusListener));
+
+        Mockito.doNothing().when(ambConversion).save();
+
+        user.setUserId("user");
+        campaign.setReferredByShortCode(null);
+
+        ambConversion.user = user;
+        ambConversion.campaign = campaign;
+        ambConversion.execute();
+
+        Mockito.verify(conversionStatusListener).pending();
+        Mockito.verify(ambConversion).save();
+    }
+
+    @Test
+    public void testsExecuteDoesPendingOnEmptyShortCode() {
+        ConversionParameters conversionParameters = new ConversionParameters.Builder()
+                .setCampaign(260)
+                .setRevenue(12)
+                .build();
+        ConversionStatusListener conversionStatusListener = Mockito.mock(ConversionStatusListener.class);
+
+        AmbConversion ambConversion = Mockito.spy(AmbConversion.get(conversionParameters, false, conversionStatusListener));
+
+        Mockito.doNothing().when(ambConversion).save();
+
+        user.setUserId("user");
+        campaign.setReferredByShortCode("");
+
+        ambConversion.user = user;
+        ambConversion.campaign = campaign;
+        ambConversion.execute();
+
+        Mockito.verify(conversionStatusListener).pending();
+        Mockito.verify(ambConversion).save();
+    }
+
+    @Test
+    public void testsExecuteDoesPendingOnOnNullUserId() {
+        ConversionParameters conversionParameters = new ConversionParameters.Builder()
+                .setCampaign(260)
+                .setRevenue(12)
+                .build();
+        ConversionStatusListener conversionStatusListener = Mockito.mock(ConversionStatusListener.class);
+
+        AmbConversion ambConversion = Mockito.spy(AmbConversion.get(conversionParameters, false, conversionStatusListener));
+
+        Mockito.doNothing().when(ambConversion).save();
+
+        user.setUserId(null);
+        campaign.setReferredByShortCode("abcd");
+
+        ambConversion.user = user;
+        ambConversion.campaign = campaign;
+        ambConversion.execute();
+
+        Mockito.verify(conversionStatusListener).pending();
+        Mockito.verify(ambConversion).save();
     }
 
 }
