625c0d67dbeb7f90c03fd5001afec4bc8ee28132
Dylan McIntyre <dylan@dmcintyre.net>
2016-05-26 10:01:18 -0400

    [#119047707] first test

--- ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/AmbassadorSDKTest.java
+++ ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/AmbassadorSDKTest.java
@@ -2,9 +2,11 @@ package com.ambassador.ambassadorsdk;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Bundle;
 
 import com.ambassador.ambassadorsdk.internal.activities.survey.SurveyModel;
 import com.ambassador.ambassadorsdk.internal.conversion.AmbConversion;
+import com.ambassador.ambassadorsdk.internal.conversion.ConversionStatusListener;
 import com.ambassador.ambassadorsdk.internal.identify.AmbIdentify;
 import com.ambassador.ambassadorsdk.internal.identify.AmbassadorIdentification;
 
@@ -14,12 +16,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest ({
+        AmbassadorSDK.class,
         AmbIdentify.class,
         AmbConversion.class,
         Color.class
@@ -33,6 +38,8 @@ public class AmbassadorSDKTest {
                 AmbConversion.class,
                 Color.class
         );
+
+        PowerMockito.spy(AmbassadorSDK.class);
 
         PowerMockito.when(AmbIdentify.class, "get", Mockito.anyString(), Mockito.any(AmbassadorIdentification.class)).thenReturn(Mockito.mock(AmbIdentify.class));
     }
@@ -62,6 +69,49 @@ public class AmbassadorSDKTest {
         boolean result = AmbassadorSDK.identify(email);
 
         Assert.assertFalse(result);
+    }
+
+    @Test
+    public void testsTrackEventThatIsNotConversionDoesNothing() throws Exception {
+        Bundle properties = Mockito.spy(Bundle.class);
+        Bundle options = Mockito.spy(Bundle.class);
+        Mockito.doReturn(false).when(options).getBoolean(Mockito.eq("conversion"), Mockito.anyBoolean());
+
+        AmbConversion ambConversion = Mockito.mock(AmbConversion.class);
+        PowerMockito.doReturn(ambConversion).when(AmbConversion.class, "get", Mockito.any(ConversionParameters.class), Mockito.anyBoolean(), Mockito.any(ConversionStatusListener.class));
+
+        final Counter counter = new Counter();
+        PowerMockito.doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                counter.increment();
+                return null;
+            }
+        }).when(AmbassadorSDK.class, "registerConversion", Mockito.any(ConversionParameters.class), Mockito.anyBoolean(), Mockito.any(ConversionStatusListener.class));
+
+        AmbassadorSDK.trackEvent("test", properties, options);
+
+        Assert.assertEquals(0, counter.getCount());
+    }
+
+    protected class Counter {
+        protected int count = 0;
+        public void increment() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+    }
+
+    @Test
+    public void testsTrackEventWithInterfaceDoesCallBack() {
+
+    }
+
+    @Test
+    public void testsTrackEventDoesConvertPropertiesToConversionParameters() {
+
     }
 
     @Test
