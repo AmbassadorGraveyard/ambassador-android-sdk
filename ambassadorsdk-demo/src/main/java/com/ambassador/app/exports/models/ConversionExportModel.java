f0e1aae2819c655e4755d6702c1dfe4aa4a16b8d
Dylan McIntyre <dylan@dmcintyre.net>
2016-06-09 15:52:58 -0400

    [#121163187] basic ConversionExportModel

---
+++ ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/models/ConversionExportModel.java
@@ -0,0 +1,13 @@
+package com.ambassador.app.exports.models;
+
+import android.os.Bundle;
+
+public class ConversionExportModel {
+
+    public Bundle conversionProperties;
+    public Bundle conversionOptions;
+    
+    public Bundle identifyTraits;
+    public Bundle identifyOptions;
+
+}
