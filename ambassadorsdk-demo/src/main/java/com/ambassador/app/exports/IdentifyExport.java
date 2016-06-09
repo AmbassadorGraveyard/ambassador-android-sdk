674d99c6c746837683886121b187fd86c14c81af
Dylan McIntyre <dylan@dmcintyre.net>
2016-06-09 10:45:35 -0400

    [#120615213] start handlebars replacement

--- ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/IdentifyExport.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/IdentifyExport.java
@@ -1,12 +1,13 @@
 package com.ambassador.app.exports;
 
+import com.ambassador.app.data.User;
 import com.ambassador.app.utils.AssetFile;
 
 public class IdentifyExport extends BaseExport<IdentifyExport> {
 
     @Override
     public String getReadme() {
-        return new AssetFile("exports/identify/readme.txt").getAsString();
+        return new AssetFile("exports/identify/readme.txt").getAsString().replaceAll("\n", "\n\n\n");
     }
 
     @Override
@@ -40,9 +41,11 @@ public class IdentifyExport extends BaseExport<IdentifyExport> {
     }
 
     protected String processHandlebars(String text) {
+        if (text == null) return text;
 
-
-        return text;
+        return text
+                .replace("{{SDKTOKEN}}", User.get().getUniversalToken())
+                .replace("{{UNIVERSALID}}", User.get().getUniversalId());
     }
 
 }
