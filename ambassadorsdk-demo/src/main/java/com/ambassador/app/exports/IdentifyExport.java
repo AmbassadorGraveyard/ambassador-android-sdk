a2b37f240789c40e6b27d1b9357ce6f4950fe691
Dylan McIntyre <dylan@dmcintyre.net>
2016-06-09 10:06:34 -0400

    [#120615213] replace code PlainTextFile impls

--- ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/IdentifyExport.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/IdentifyExport.java
@@ -1,6 +1,5 @@
 package com.ambassador.app.exports;
 
-import com.ambassador.app.data.User;
 import com.ambassador.app.utils.AssetFile;
 
 public class IdentifyExport extends BaseExport<IdentifyExport> {
@@ -31,69 +30,17 @@ public class IdentifyExport extends BaseExport<IdentifyExport> {
 
     @Override
     public String getJavaImplementation() {
-        PlaintextFile java = new PlaintextFile();
-        java.addLine("package com.example.example;");
-        java.addLine("");
-        java.addLine("import android.app.Application;");
-        java.addLine("import com.ambassador.ambassadorsdk.AmbassadorSDK;");
-        java.addLine("");
-        java.addLine("public class MyApplication extends Application {");
-        java.addLine("");
-        java.addLineWithPadding(4, "@Override");
-        java.addLineWithPadding(4, "public void onCreate() {");
-        java.addLineWithPadding(8, "super.onCreate();");
-        java.addLineWithPadding(8, String.format("AmbassadorSDK.runWithKeys(this, \"SDKToken %s\", \"%s\");", User.get().getSdkToken(), User.get().getUniversalId()));
-        java.addLineWithPadding(8, String.format("AmbassadorSDK.identify(\"%s\");", model));
-        java.addLineWithPadding(4, "}");
-        java.addLine("");
-        java.addLine("}");
-
         return new AssetFile("exports/identify/MyApplication.java").getAsString();
     }
 
     @Override
     public String getSwiftImplementation() {
-        PlaintextFile swift = new PlaintextFile();
-        swift.addLine("import UIKit");
-        swift.addLine("");
-        swift.addLine("@UIApplicationMainclass AppDelegate: UIResponder, UIApplicationDelegate {");
-        swift.addLine("");
-        swift.addLineWithPadding(4, "var window: UIWindow?");
-        swift.addLine("");
-        swift.addLineWithPadding(4, "func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {");
-        swift.addLineWithPadding(8, String.format("AmbassadorSDK.runWithUniversalToken(\"%s\", universalID: \"%s\")", User.get().getSdkToken(), User.get().getUniversalId()));
-        swift.addLineWithPadding(8, String.format("AmbassadorSDK.identifyWithEmail(\"%s\")", model));
-        swift.addLineWithPadding(8, "");
-        swift.addLineWithPadding(8, "return true");
-        swift.addLineWithPadding(4, "}");
-        swift.addLine("");
-        swift.addLine("}");
-
-        return swift.get();
+        return new AssetFile("exports/identify/AppDelegate.swift").getAsString();
     }
 
     @Override
     public String getObjectiveCImplementation() {
-        PlaintextFile objc = new PlaintextFile();
-        objc.addLine("#import \"AppDelegate.h\"");
-        objc.addLine("#import <Ambassador/Ambassador.h>");
-        objc.addLine("");
-        objc.addLine("@interface AppDelegate ()");
-        objc.addLine("");
-        objc.addLine("@end");
-        objc.addLine("");
-        objc.addLine("@implementation AppDelegate");
-        objc.addLine("");
-        objc.addLine("- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {");
-        objc.addLineWithPadding(4, String.format("[AmbassadorSDK runWithUniversalToken:\"%s\" universalID:\"%s\"];", User.get().getSdkToken(), User.get().getUniversalId()));
-        objc.addLineWithPadding(4, String.format("[AmbassadorSDK identifyWithEmail:@\"%s\"];", model));
-        objc.addLineWithPadding(4, "");
-        objc.addLineWithPadding(4, "return YES;");
-        objc.addLine("}");
-        objc.addLine("");
-        objc.addLine("@end");
-
-        return objc.get();
+        return new AssetFile("exports/identify/AppDelegate.m").getAsString();
     }
 
     @Override
