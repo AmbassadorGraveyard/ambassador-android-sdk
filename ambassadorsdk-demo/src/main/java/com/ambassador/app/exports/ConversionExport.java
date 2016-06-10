da032b13359125c8a42b73bb927a182fcb8722af
Dylan McIntyre <dylan@dmcintyre.net>
2016-06-10 16:59:50 -0400

    [#121163187] processing

--- ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/ConversionExport.java
+++ ambassadorsdk-demo/src/main/java/com/ambassador/app/exports/ConversionExport.java
@@ -1,5 +1,8 @@
 package com.ambassador.app.exports;
 
+import android.os.Bundle;
+
+import com.ambassador.app.data.User;
 import com.ambassador.app.exports.models.ConversionExportModel;
 import com.ambassador.app.utils.AssetFile;
 
@@ -16,17 +19,17 @@ public class ConversionExport extends BaseExport<ConversionExportModel> {
 
     @Override
     public String getJavaImplementation() {
-        return new AssetFile("exports/conversion/MyApplication.java").getAsString();
+        return processVariableHandlebarValues(processConstantHandlebarValues(new AssetFile("exports/conversion/MyApplication.java").getAsString()), Language.JAVA);
     }
 
     @Override
     public String getSwiftImplementation() {
-        return new AssetFile("exports/conversion/AppDelegate.swift").getAsString();
+        return processVariableHandlebarValues(processConstantHandlebarValues(new AssetFile("exports/conversion/AppDelegate.swift").getAsString()), Language.SWIFT);
     }
 
     @Override
     public String getObjectiveCImplementation() {
-        return new AssetFile("exports/conversion/AppDelegate.m").getAsString();
+        return processVariableHandlebarValues(processConstantHandlebarValues(new AssetFile("exports/conversion/AppDelegate.m").getAsString()), Language.OBJ_C);
     }
 
     @Override
@@ -45,12 +48,25 @@ public class ConversionExport extends BaseExport<ConversionExportModel> {
     }
 
     protected String processConstantHandlebarValues(String text) {
+        removeNullInstancesInPlace(model);
 
+        Bundle conversionProperties = model.conversionProperties;
+        Bundle conversionOptions = model.conversionOptions;
+        Bundle identifyTraits = model.identifyTraits;
+        Bundle identifyOptions = model.identifyOptions;
 
-        return "";
+        return text
+                .replace("{{SDKTOKEN}}", User.get().getUniversalToken())
+                .replace("{{UNIVERSALID}}", User.get().getUniversalId())
+                .replace("{{USERID}}", "");
     }
 
     protected String processVariableHandlebarValues(String text, Language language) {
+        Bundle conversionProperties = model.conversionProperties;
+        Bundle conversionOptions = model.conversionOptions;
+        Bundle identifyTraits = model.identifyTraits;
+        Bundle identifyOptions = model.identifyOptions;
+
         switch (language) {
             case JAVA:
                 break;
@@ -61,7 +77,50 @@ public class ConversionExport extends BaseExport<ConversionExportModel> {
             default:
                 return null;
         }
-        return "";
+        return text;
+    }
+
+    protected void removeNullInstancesInPlace(ConversionExportModel model) {
+        if (model.conversionProperties == null) {
+            model.conversionProperties = new Bundle();
+        }
+        Bundle conversionProperties = model.conversionProperties;
+
+        if (model.conversionOptions == null) {
+            model.conversionOptions = new Bundle();
+        }
+        Bundle conversionOptions = model.conversionOptions;
+
+        if (model.identifyTraits == null) {
+            model.identifyTraits = new Bundle();
+        }
+        Bundle identifyTraits = model.identifyTraits;
+
+        if (model.identifyOptions == null) {
+            model.identifyOptions = new Bundle();
+        }
+        Bundle identifyOptions = model.identifyOptions;
+
+        removeBundleNullInstance(identifyTraits, "email");
+        removeBundleNullInstance(identifyTraits, "firstName");
+        removeBundleNullInstance(identifyTraits, "lastName");
+        removeBundleNullInstance(identifyTraits, "addToGroups");
+        removeBundleNullInstance(identifyTraits, "customLabel1");
+        removeBundleNullInstance(identifyTraits, "customLabel2");
+        removeBundleNullInstance(identifyTraits, "customLabel3");
+
+        removeBundleNullInstance(identifyOptions, "campaign");
+
+        removeBundleNullInstance(conversionProperties, "eventData1");
+        removeBundleNullInstance(conversionProperties, "eventData2");
+        removeBundleNullInstance(conversionProperties, "eventData3");
+        removeBundleNullInstance(conversionProperties, "orderId");
+    }
+
+    protected void removeBundleNullInstance(Bundle bundle, String key) {
+        if (bundle.getString(key, null) == null) {
+            bundle.putString(key, "");
+        }
     }
 
 }
