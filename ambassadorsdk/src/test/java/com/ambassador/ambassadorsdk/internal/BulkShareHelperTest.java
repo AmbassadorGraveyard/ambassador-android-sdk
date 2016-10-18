434cba4511db32792da432cb41d738914d603a11
Corey Fields <corey@getambassador.com>
2016-10-18 17:42:41 -0400

    fix tests

--- ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/internal/BulkShareHelperTest.java
+++ ambassadorsdk/src/test/java/com/ambassador/ambassadorsdk/internal/BulkShareHelperTest.java
@@ -1,8 +1,6 @@
 package com.ambassador.ambassadorsdk.internal;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 
 import com.ambassador.ambassadorsdk.internal.api.RequestManager;
@@ -89,32 +87,6 @@ public class BulkShareHelperTest {
         // ACT & ASSERT
         bulkShareHelper.bulkShare(message, contacts, true, bulkShareCompletion);
         Mockito.verify(bulkShareCompletion).bulkShareFailure();
-    }
-
-    @Test
-    public void bulkShareSMSApi21SingleContactTest() throws Exception {
-        // ARRANGE
-        String message = "message";
-        List<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact.Builder().setPhoneNumber("3133291104").build());
-        BulkShareHelper.BulkShareCompletion bulkShareCompletion = Mockito.mock(BulkShareHelper.BulkShareCompletion.class);
-        Mockito.doReturn(21).when(device).getSdkVersion();
-        Context context = Mockito.mock(Context.class);
-        Mockito.when(AmbSingleton.getContext()).thenReturn(context);
-        PackageManager packageManager = Mockito.mock(PackageManager.class);
-        Mockito.when(context.getPackageManager()).thenReturn(packageManager);
-        Mockito.doReturn(true).when(packageManager).hasSystemFeature(Mockito.anyString());
-
-        PowerMockito.doReturn(Mockito.mock(Uri.class)).when(Uri.class, "parse", Mockito.anyString());
-        Intent intent = Mockito.mock(Intent.class);
-        PowerMockito.whenNew(Intent.class).withAnyArguments().thenReturn(intent);
-
-        // ACT
-        bulkShareHelper.bulkShare(message, contacts, true, bulkShareCompletion);
-
-        // ASSERT
-        Mockito.verify(bulkShareCompletion).launchSmsIntent(Mockito.eq("3133291104"), Mockito.eq(intent));
-        Mockito.verify(intent).putExtra(Mockito.eq("sms_body"), Mockito.eq(message));
     }
 
     @Test
