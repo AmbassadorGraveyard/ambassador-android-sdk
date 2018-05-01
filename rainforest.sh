88c31fd0ce6072aa5cce6c7e85711192d8411713
jarell-lloyd <jarell.lloyd@gmail.com>
2018-05-01 15:33:38 -0400

    setting up rainforest

--- rainforest.sh
+++ rainforest.sh
@@ -1,17 +1,45 @@
 #!/bin/sh
+# Builds a debug APK and tests APK for the demo app and uploads them
+# to AWS S3. Runs tests and reports status to GitHub.
 
-# fail if err occurs on any step
-set -o errexit
+# Call abort on exit
+trap 'abort' 0;
 
-if [ "$CIRCLE_BRANCH" != "master" ]
-  then
-    APK_NAME="rainforest.apk";
+# Fail on any command error
+set -e;
+
+# Get the commit msg
+msg=`git log -1 --pretty=%B`;
+
+# Get commit sha for GitHub reporting
+sha=`git rev-parse HEAD`;
+
+# Get the branch name
+branch=`git rev-parse --abbrev-ref HEAD`;
+
+# If "RunUiTests" in commit msg or branch is master
+if [[ $msg == *"@RunUiTests"* ]] || [ $branch == "master" ]
+then
+
+    # Create new name for app APK as current epoch time + '.apk'
+    APK_NAME="ambassadorsdk-demo-debug.apk";
+
+    # Copy APK to artifacts directory with new name
+    echo "Copying debug apk to $CIRCLE_ARTIFACTS/$APK_NAME";
     cp ./ambassadorsdk-demo/build/outputs/apk/ambassadorsdk-demo-debug.apk $CIRCLE_ARTIFACTS/$APK_NAME;
-    curl -H "Authorization: Bearer $DROPBOX_TOKEN" https://api-content.dropbox.com/1/files_put/auto/ -T $CIRCLE_ARTIFACTS/$APK_NAME
 
-    rainforest validate --token $RAINFOREST_TOKEN
-    rainforest upload --token $RAINFOREST_TOKEN
+    # Upload Debug APK file to S3
+    echo "Requesting upload to device farm";
+    aws aws s3 cp --acl public-read $CIRCLE_ARTIFACTS/$APK_NAME s3://ambassador-rainforest/android/debug/DemoApp.apk;
 
-    GITHUB_PULL_NUMBER=$(echo $CI_PULL_REQUEST | awk -F/ '{print $7}')
-    rainforest run all --fg --fail-fast --git-trigger --token "$RAINFOREST_TOKEN" --site-id "$RAINFOREST_SITE" --description "CI run for $CIRCLE_BRANCH" --browsers android_phone_portrait
+    # Trigger RainforestQA test run
+    rainforest run --run-group 1793 --fail-fast --environment-id 5165 --token "$RAINFOREST_TOKEN" --description "Ambassador Android Demo App automated post deploy run" --bg
+
+
+else
+    # Clarify in CircleCI why devicefarm.sh did nothing on this commit.
+    echo "Tests not running. To run tests outside of master add @RunUiTests to the commit message.";
 fi
+
+# End abort on exit block
+trap : 0
